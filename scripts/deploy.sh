c69a0728d6efd8244081b981a6363fb7beae1b6c
Daniel Bosen <daniel@bosen.eu>
2015-10-01 11:38:13 +0200

    use after_success instead of deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,16 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-echo "deployment"
-echo $PROJEKT
-echo $TRAVIS_TAG
-echo $TRAVIS_BRANCH
+if [ $TRAVIS_PULL_REQUEST == "true" ]
+then
+    echo "Build successful, pull requests will not be deployed"
+fi
+
+if [ $TRAVIS_BRANCH == "master" ]
+then
+  echo 'Deploy to stage.'
+elif [ $TRAVIS_BRANCH == "develop" ]
+then
+  echo 'Deploy to testing.'
+else
+   echo "Build successful, $TRAVIS_BRANCH will not be deployed"
+fi
