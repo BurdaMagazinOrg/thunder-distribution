b76f37ea792a3ac516f03ae5e4ccef3cbcf85729
Daniel Bosen <daniel@bosen.eu>
2015-10-02 11:51:26 +0200

    initial acquia connection

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,21 @@
 if [ $TRAVIS_PULL_REQUEST == "true" ]
 then
     echo "Build successful, pull requests will not be deployed"
+    exit
 fi
+
+ssh-keyscan $ACQUIA_HOST >> ~/.ssh/known_hosts
 
 if [ $TRAVIS_BRANCH == "master" ]
 then
   echo 'Deploy to stage.'
+  git clone --branch $TRAVIS_BRANCH $ACQUIA_REPOSITORY acquia
+  ls -l acquia
 elif [ $TRAVIS_BRANCH == "develop" ]
 then
   echo 'Deploy to testing.'
+  git clone --branch $TRAVIS_BRANCH $ACQUIA_REPOSITORY acquia
+  ls -l acquia
 else
    echo "Build successful, $TRAVIS_BRANCH will not be deployed"
 fi
