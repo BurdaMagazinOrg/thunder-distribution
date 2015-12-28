f1e61eba705243e72f84bdfbe38bdc4a8a3b9a0e
Daniel Bosen <daniel@bosen.eu>
2015-12-28 10:24:43 +0100

    update text

--- travis/scripts/deploy.sh
+++ travis/scripts/deploy.sh
@@ -55,13 +55,13 @@ function deploy_to_acquia() {
 
 if [ $ACQUIA_REPOSITORY == "" ]
 then
-    echo "Build successful, pull requests can not be deployed, please provide $ACQUIA_REPOSITORY environment variable."
+    echo "Build successful, commit can not be deployed, please provide $ACQUIA_REPOSITORY environment variable."
     exit
 fi
 
 if [ $ACQUIA_HOST == "" ]
 then
-    echo "Build successful, pull requests can not be deployed, please provide $ACQUIA_HOST environment variable."
+    echo "Build successful, commit can not be deployed, please provide $ACQUIA_HOST environment variable."
     exit
 fi
 
