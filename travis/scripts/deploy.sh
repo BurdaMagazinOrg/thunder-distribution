413337dbfcd9c6e0fa02bb7b568962bb808c1f00
Daniel Bosen <daniel@bosen.eu>
2015-12-29 10:19:35 +0100

    white space fixes

--- travis/scripts/deploy.sh
+++ travis/scripts/deploy.sh
@@ -1,67 +1,66 @@
 #!/bin/bash
 
 function deploy_to_acquia() {
-   echo "Deploying $TRAVIS_BRANCH"
+    echo "Deploying $TRAVIS_BRANCH"
 
-   cd $TRAVIS_BUILD_DIR
-   LAST_COMMIT_INFO=$(git log -1 --pretty="[%h] (%an) %B")
-   LAST_COMMIT_USER=$(git show -s --format="%an")
-   LAST_COMMIT_USER_EMAIL=$(git show -s --format="%ae")
-   if [ "$TRAVIS_TAG" == "" ]
-   then
-    COMMIT_TAG=$(git tag --points-at $TRAVIS_COMMIT)
-   else
-    COMMIT_TAG=$TRAVIS_TAG
-   fi
-
-   chmod a+rwx docroot/sites/default/settings.php
-   chmod a+rwx docroot/sites/default
-   cp -v settings/settings.acquia.php docroot/sites/default/settings.php
-
-   # FIXME workarounds for problematic config management (no stage dependent config) - remove these when issue is fixed
-   cp -v settings/settings.dev.php docroot/sites/default/settings.dev.php
-   cp -v settings/settings.prod.php docroot/sites/default/settings.prod.php
-
-   rm docroot/sites/default/settings.local.php
-
-   git clone $ACQUIA_REPOSITORY acquia
-   cd acquia
-
-   if [ "$COMMIT_TAG" == "" ]
-   then
-    git rev-parse --verify testing > /dev/null 2>&1
-    if [ "$?" = "0" ]
+    cd $TRAVIS_BUILD_DIR
+    LAST_COMMIT_INFO=$(git log -1 --pretty="[%h] (%an) %B")
+    LAST_COMMIT_USER=$(git show -s --format="%an")
+    LAST_COMMIT_USER_EMAIL=$(git show -s --format="%ae")
+    if [ "$TRAVIS_TAG" == "" ]
     then
-        git checkout $TRAVIS_BRANCH
+        COMMIT_TAG=$(git tag --points-at $TRAVIS_COMMIT)
     else
-        git checkout -b $TRAVIS_BRANCH
-        git push -u origin $TRAVIS_BRANCH
+        COMMIT_TAG=$TRAVIS_TAG
     fi
-   fi
 
-   mkdir -p config
+    chmod a+rwx docroot/sites/default/settings.php
+    chmod a+rwx docroot/sites/default
+    cp -v settings/settings.acquia.php docroot/sites/default/settings.php
 
-   rsync -ah --delete ../docroot/ docroot/
-   rsync -ah --delete ../config/staging/ config/staging/
+    # FIXME workarounds for problematic config management (no stage dependent config) - remove these when issue is fixed
+    cp -v settings/settings.dev.php docroot/sites/default/settings.dev.php
+    cp -v settings/settings.prod.php docroot/sites/default/settings.prod.php
 
+    rm docroot/sites/default/settings.local.php
 
-   # do not fix line endings, keep everything as is
-   echo "* -text" > docroot/.gitattributes
+    git clone $ACQUIA_REPOSITORY acquia
+    cd acquia
 
-   git config user.email "$LAST_COMMIT_USER_EMAIL"
-   git config user.name "$LAST_COMMIT_USER"
-   git config --global push.default simple
+    if [ "$COMMIT_TAG" == "" ]
+    then
+        git rev-parse --verify testing > /dev/null 2>&1
+        if [ "$?" = "0" ]
+        then
+            git checkout $TRAVIS_BRANCH
+        else
+            git checkout -b $TRAVIS_BRANCH
+            git push -u origin $TRAVIS_BRANCH
+        fi
+    fi
 
-   git add --all .
-   git commit --quiet -m "$LAST_COMMIT_INFO"
+    mkdir -p config
 
-   if [ "$COMMIT_TAG" != "" ]
-   then
-    git tag $COMMIT_TAG
-    git push origin $COMMIT_TAG
-   else
-    git push origin $TRAVIS_BRANCH
-   fi
+    rsync -ah --delete ../docroot/ docroot/
+    rsync -ah --delete ../config/staging/ config/staging/
+
+    # do not fix line endings, keep everything as is
+    echo "* -text" > docroot/.gitattributes
+
+    git config user.email "$LAST_COMMIT_USER_EMAIL"
+    git config user.name "$LAST_COMMIT_USER"
+    git config --global push.default simple
+
+    git add --all .
+    git commit --quiet -m "$LAST_COMMIT_INFO"
+
+    if [ "$COMMIT_TAG" != "" ]
+        then
+        git tag $COMMIT_TAG
+        git push origin $COMMIT_TAG
+    else
+        git push origin $TRAVIS_BRANCH
+    fi
 }
 
 if [ $ACQUIA_REPOSITORY == "" ]
@@ -83,5 +82,4 @@ then
 fi
 
 ssh-keyscan $ACQUIA_HOST >> ~/.ssh/known_hosts
-
 deploy_to_acquia
