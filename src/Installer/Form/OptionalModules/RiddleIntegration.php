713f1a8c7887f6fc5fb09c28b24d402ba641fd36
Christian Fritsch <chr.fritsch@gmx.net>
2016-02-25 09:53:28 +0100

    Use riddle paragraphs module

--- src/Installer/Form/OptionalModules/RiddleIntegration.php
+++ src/Installer/Form/OptionalModules/RiddleIntegration.php
@@ -15,7 +15,7 @@ class RiddleIntegration extends AbstractOptionalModule {
 
   public function getFormId() {
 
-    return 'riddle_marketplace';
+    return 'paragraphs_riddle_marketplace';
   }
 
   public function getFormName() {
@@ -33,18 +33,18 @@ class RiddleIntegration extends AbstractOptionalModule {
 
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $form['riddle_marketplace'] = array(
+    $form['paragraphs_riddle_marketplace'] = array(
       '#type' => 'details',
       '#title' => $this->t('Riddle'),
       '#open' => TRUE,
       '#states' => array(
         'visible' => array(
-          ':input[name="install_modules[riddle_marketplace]"]' => array('checked' => TRUE),
+          ':input[name="install_modules[paragraphs_riddle_marketplace]"]' => array('checked' => TRUE),
         ),
       )
     );
 
-    $form['riddle_marketplace']['riddle_token'] = array(
+    $form['paragraphs_riddle_marketplace']['riddle_token'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Riddle token'),
       '#description' => $this->t('Goto Riddle.com and get a token from the Account->Plugins page (you may need to reset to get the first token)'),
@@ -58,5 +58,18 @@ class RiddleIntegration extends AbstractOptionalModule {
     $this->config('riddle_marketplace.settings')
       ->set('token', (string) $form_state->getValue('riddle_token'))
       ->save(TRUE);
+
+    /** @var \Drupal\field\Entity\FieldConfig $field */
+    $field = entity_load('field_config', 'node.article.field_paragraphs');
+
+    $settings = $field->getSetting('handler_settings');
+
+    $settings['target_bundles']['paragraphs_riddle_marketplace'] = 'paragraphs_riddle_marketplace';
+    $settings['target_bundles_drag_drop']['paragraphs_riddle_marketplace'] = ['enabled' => TRUE, 'weight' => 10];
+
+    $field->setSetting('handler_settings', $settings);
+
+    $field->save();
+
   }
 }
