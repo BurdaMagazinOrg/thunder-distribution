21e648261767d3914149c4e888effd2e028ed79c
Daniel Bosen <daniel.bosen@burda.com>
2016-03-17 15:21:05 -0400

    completely remove old structure

--- settings/example.settings.prod.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Needed to avoid duplicate content from other domains / protocols.
-#$base_url = 'http://your.domain.tld';
-
-// Disable SFP handling.
-#$config['stage_file_proxy.settings']['origin'] = '';
