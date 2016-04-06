c2237beed86293a97aee672497b41951b77367b1
Christian Fritsch <chr.fritsch@gmx.net>
2016-04-06 10:25:11 +0200

    Add configuration form to disable/enable filename cleaning

---
+++ modules/thunder_media/src/Form/ConfigurationForm.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\thunder_media\Form\ConfigurationForm.
+ */
+
+namespace Drupal\thunder_media\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class ConfigurationForm.
+ *
+ * @package Drupal\thunder_media\Form
+ */
+class ConfigurationForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'thunder_media.configuration',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'configuration_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('thunder_media.configuration');
+
+    $form['enable_filename_transliteration'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Enable filename transliteration'),
+      '#description' => t('Enable this checkbox to clean filenames before saving the files.'),
+      '#default_value' => $config->get('enable_filename_transliteration')
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('thunder_media.configuration')
+      ->set('enable_filename_transliteration', $form_state->getValue('enable_filename_transliteration'))
+      ->save();
+  }
+
+}
