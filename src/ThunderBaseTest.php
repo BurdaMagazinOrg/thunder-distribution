4612bc12878193cf00caa9451768cd5b35e1d45e
Christian Fritsch <chr.fritsch@gmx.net>
2016-05-10 12:39:17 +0200

    Add media_entity_instagram and media_entity_twitter
    * Add media_entity_instagram and media_entity_twitter
    
    * Update travis-ci
    
    * Add ief getButtonName function

--- src/ThunderBaseTest.php
+++ src/ThunderBaseTest.php
@@ -15,4 +15,26 @@ class ThunderBaseTest extends WebTestBase {
 
   protected $strictConfigSchema = FALSE;
 
+  /**
+   * Gets IEF button name.
+   *
+   * @param array $xpath
+   *   Xpath of the button.
+   *
+   * @return string
+   *   The name of the button.
+   */
+  protected function getButtonName($xpath) {
+    $retval = '';
+    /** @var \SimpleXMLElement[] $elements */
+    if ($elements = $this->xpath($xpath)) {
+      foreach ($elements[0]->attributes() as $name => $value) {
+        if ($name == 'name') {
+          $retval = $value;
+          break;
+        }
+      }
+    }
+    return $retval;
+  }
 }
