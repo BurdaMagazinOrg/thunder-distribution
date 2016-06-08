257a996e260944117710a2ef93e3162ef854ca8c
Christian Fritsch <chr.fritsch@gmx.net>
2016-06-08 17:11:56 +0200

    Fix build script

--- scripts/build-docroot.sh
+++ scripts/build-docroot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $INSTALL_METHOD eq "drush_make" ]
+if [ "$INSTALL_METHOD" eq "drush_make" ]
 then
   # Build drupal + thunder from makefile
   drush make --concurrency=5 drupal-org-core.make docroot -y
@@ -10,7 +10,7 @@ then
   drush make -y --no-core docroot/profiles/thunder/drupal-org.make docroot/profiles/thunder
 fi
 
-if [ $INSTALL_METHOD eq "composer" ]
+if [ "$INSTALL_METHOD" eq "composer" ]
 then
     composer create-project burdamagazinorg/thunder-infrastructure test-dir --stability dev --no-interaction
     cd test-dir
