d3a5c9389881a048952f20c15e8a11a6bba10607
Mladen Todorovic <mladen.todorovic@burda.com>
2016-07-01 14:12:37 +0200

    Adjusted composer build

--- scripts/build-docroot.sh
+++ scripts/build-docroot.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+
 if [ "$INSTALL_METHOD" == "drush_make" ]
 then
   # Build drupal + thunder from makefile
-  drush make --concurrency=5 drupal-org-core.make test-dir/docroot -y
-  mkdir test-dir/docroot/profiles/thunder
+  drush make --concurrency=5 drupal-org-core.make ${TEST_DIR}/docroot -y
+  mkdir ${TEST_DIR}/docroot/profiles/thunder
   shopt -s extglob
-  rsync -a --progress . test-dir/docroot/profiles/thunder --exclude docroot
-  drush make -y --no-core test-dir/docroot/profiles/thunder/drupal-org.make test-dir/docroot/profiles/thunder
+  rsync -a --progress . ${TEST_DIR}/docroot/profiles/thunder --exclude docroot
+  drush make -y --no-core ${TEST_DIR}/docroot/profiles/thunder/drupal-org.make ${TEST_DIR}/docroot/profiles/thunder
 fi
 
 if [ "$INSTALL_METHOD" == "composer" ]
 then
-    composer create-project burdamagazinorg/thunder-infrastructure test-dir --stability dev --no-interaction
-    cd test-dir
-    composer require burdamagazinorg/thunder:dev-8.x-1.x
-    composer update
+    composer create-project burdamagazinorg/thunder-infrastructure ${TEST_DIR} --stability dev --no-interaction --no-install
+    cd ${TEST_DIR}
+    composer config repositories.thunder path ${THUNDER_DIST_DIR}
+    composer require "burdamagazinorg/thunder:*" --no-progress
 fi
