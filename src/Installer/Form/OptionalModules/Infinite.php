f5a50d1615b8789f078dc98032b67588bf749069
Christian Fritsch <chr.fritsch@gmx.net>
2016-07-04 16:41:13 +0200

    Remove infinite stuff, add bartik blocks, update default content

---
+++ src/Installer/Form/OptionalModules/Infinite.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\thunder\Installer\Form\OptionalModules;
+
+/**
+ * @file
+ * Contains
+ */
+class Infinite extends AbstractOptionalModule {
+
+  public function getFormId() {
+
+    return 'infinite_article';
+  }
+
+  public function getFormName() {
+    return 'Infinite theme';
+  }
+}
