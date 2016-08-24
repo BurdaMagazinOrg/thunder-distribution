c9eaaa8b884c6a82ec0a44cbf5f8a6e3e59b9e62
Daniel Bosen <daniel@bosen.eu>
2016-08-24 14:48:38 +0200

    Feature/travis improvements (#85)
    * test simpletest and behat seperately
    * add output every minute
    * remove run-tests script

--- scripts/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# run Drupal tests (@group Thunder)
-php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --die-on-fail --verbose --url http://localhost:8080 Thunder
-
-# run Behat tests
-behat --config ${THUNDER_DIST_DIR}/tests/behat/behat.travis.yml
