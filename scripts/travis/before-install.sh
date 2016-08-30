df2459a55959e4d7a2e39163ae547b96fa3b2609
Daniel Bosen <daniel.bosen@burda.com>
2016-08-30 10:25:51 +0200

    source environment

--- scripts/travis/before-install.sh
+++ scripts/travis/before-install.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-
-## Setup environment
-export PHANTOMJS_VERSION=2.1.1
-export PATH=$PWD/travis_phantomjs/phantomjs-${PHANTOMJS_VERSION}-linux-x86_64/bin:$PATH
-
-# add composer's global bin directory to the path
-# see: https://github.com/drush-ops/drush#install---composer
-export PATH="$HOME/.composer/vendor/bin:$PATH"
-export THUNDER_DIST_DIR=`echo $(pwd)`
-export TEST_DIR=`echo ${THUNDER_DIST_DIR}"/../test-dir"`
-
-
 # remove xdebug to make php execute faster
 phpenv config-rm xdebug.ini
 
