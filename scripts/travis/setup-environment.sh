7e7a58acb5b107c9a69e24536119f1c26f8a223f
Daniel Bosen <daniel.bosen@burda.com>
2016-08-30 13:23:44 +0200

    move phantomjs to tests setup script

--- scripts/travis/setup-environment.sh
+++ scripts/travis/setup-environment.sh
@@ -9,3 +9,11 @@ export PATH=$PWD/travis_phantomjs/phantomjs-${PHANTOMJS_VERSION}-linux-x86_64/bi
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 export THUNDER_DIST_DIR=`echo $(pwd)`
 export TEST_DIR=`echo ${THUNDER_DIST_DIR}"/../test-dir"`
+
+# depending on install method, the composer vendor dirrectory is in different places
+if [[ ${INSTALL_METHOD} == "drush_make" ]]; then
+    COMPOSER_VENDOR_DIR=${TEST_DIR}/docroot/vendor
+elif [[ ${INSTALL_METHOD} == "composer" ]]; then
+    COMPOSER_VENDOR_DIR=${TEST_DIR}/vendor
+fi
+export COMPOSER_DIR
