9d15207280fc147303178bd3f1257c5fa207d125
Daniel Bosen <daniel.bosen@burda.com>
2016-09-01 16:38:03 +0200

    colored output

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -2,4 +2,4 @@
 
 # Run Drupal tests (@group Thunder)
 cd ${TEST_DIR}/docroot
-php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --die-on-fail --verbose --url http://localhost:8080 Thunder
+php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --die-on-fail --verbose --color --url http://localhost:8080 Thunder
