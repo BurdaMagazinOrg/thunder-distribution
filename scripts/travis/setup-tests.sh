0f3acd0147009ebf147fbb5b62bdc328e9dcdaaf
Daniel Bosen <daniel.bosen@burda.com>
2016-09-05 09:24:13 +0200

    reenable drush runserver

--- scripts/travis/setup-tests.sh
+++ scripts/travis/setup-tests.sh
@@ -6,5 +6,8 @@ cd ${TEST_DIR}/docroot
 # Final cache rebuild, to make sure every code change is respected
 drush cr
 
+# Run the webserver
+drush runserver --default-server=builtin 8080 &>/dev/null &
+
 # Run phantomjs for javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true ${LOCAL_COMPOSER_VENDOR_DIR}/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 2250 &>/dev/null &
