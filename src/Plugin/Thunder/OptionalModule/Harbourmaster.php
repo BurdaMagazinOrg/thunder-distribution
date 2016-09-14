4912b1c7ae36167e450bd3cfba26c1153886280a
Daniel Bosen <daniel@bosen.eu>
2016-09-14 17:46:19 +0200

    Add harbourmaster sso module
    * add valiton harbourmaster module
    
    * add optional module and drush make
    
    * fix class name
    
    * download beta4
    
    * Fix coding styles

---
+++ src/Plugin/Thunder/OptionalModule/Harbourmaster.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\thunder\Plugin\Thunder\OptionalModule;
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Harbourmaster.
+ *
+ * @ThunderOptionalModule(
+ *   id = "harbourmaster",
+ *   label = @Translation("Harbourmaster"),
+ *   type = "module",
+ * )
+ */
+class Harbourmaster extends AbstractOptionalModule {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['harbourmaster'] = array(
+      '#type' => 'details',
+      '#title' => $this->t('Harbourmaster SSO connector'),
+      '#open' => TRUE,
+      '#states' => array(
+        'visible' => array(
+          ':input[name="install_modules[harbourmaster]"]' => array('checked' => TRUE),
+        ),
+      ),
+      '#description' => t('Integrates Harbourmaster providing a single sign-on solution for Drupal. You will need an instance of the harbourmaster running. See <a href=":url" target="_blank">harbourmaster documentation</a> for more details.', [
+        ':url' => 'https://valiton.github.io/harbourmaster-docs/index.html',
+      ]),
+    );
+
+    return $form;
+  }
+
+}
