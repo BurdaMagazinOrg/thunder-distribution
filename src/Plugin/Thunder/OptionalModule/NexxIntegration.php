9e456c19f47d39d537f48ec31926f1d69ca45680
Daniel Bosen <daniel.bosen@burda.com>
2016-09-19 13:10:25 +0200

    documentation fix

--- src/Plugin/Thunder/OptionalModule/NexxIntegration.php
+++ src/Plugin/Thunder/OptionalModule/NexxIntegration.php
@@ -24,7 +24,7 @@ class NexxIntegration extends AbstractOptionalModule {
       '#type' => 'details',
       '#title' => $this->t('Nexx video'),
       '#open' => TRUE,
-      '#description' => $this->t('Register a new account at <a href=":nexx_url" target="_blank">http://www.nexx.tv/thunder</a> and get a domain ID and an installation code. You can provide theme right here or at a later stage on the nexx Settings form',
+      '#description' => $this->t('Register a new account at <a href=":nexx_url" target="_blank">http://www.nexx.tv/thunder</a> and get a domain ID and an installation code. You can provide them right here or at a later stage on the nexx Settings form.',
         [':nexx_url' => 'http://www.nexx.tv/thunder']),
       '#states' => array(
         'visible' => array(
