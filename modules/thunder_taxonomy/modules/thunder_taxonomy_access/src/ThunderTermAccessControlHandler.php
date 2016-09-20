4006ff1f943d6016b51d0af3b333234e2b618716
Steffen Rühlmann <steffen@undpaul.de>
2016-09-20 13:18:54 +0200

    #2802805: updated ThunderTermAccessControlHandler to support new view published/ unpublished permissions

--- modules/thunder_taxonomy/modules/thunder_taxonomy_access/src/ThunderTermAccessControlHandler.php
+++ modules/thunder_taxonomy/modules/thunder_taxonomy_access/src/ThunderTermAccessControlHandler.php
@@ -21,12 +21,13 @@ class ThunderTermAccessControlHandler extends TermAccessControlHandler {
 
     switch ($operation) {
       case 'view':
-        return AccessResult::allowedIf($account->hasPermission('access content') && $entity->status->value);
+        // Check for status and set 'published' or 'unpublished'
+        $status = ($entity->status->value)  ? 'published' : 'unpublished';
+        return AccessResult::allowedIf($account->hasPermission('access content') && $account->hasPermission('view ' . $status . ' terms in ' . $entity->bundle()));
 
       default:
         return parent::checkAccess($entity, $operation, $account);
 
     }
   }
-
 }
