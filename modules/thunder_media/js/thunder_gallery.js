670b759ba2ccfb18b996c136dae44e855b7cb8f2
Mladen Todorovic <mladen.todorovic@burda.com>
2016-09-22 15:10:25 +0200

    Code style adjusted for JS files

--- modules/thunder_media/js/thunder_gallery.js
+++ modules/thunder_media/js/thunder_gallery.js
@@ -4,16 +4,16 @@
  * Media related javascripts.
  */
 (function ($) {
-    'use strict';
+  'use strict';
 
-    /**
-     * Registers behaviours related to thunder media.
-     */
-    Drupal.behaviors.fixGallery = {
-        attach: function (context) {
-            $('.media-gallery img').height($('.media-gallery img').attr('height'));
-        }
-    };
+  /**
+   * Registers behaviours related to thunder media.
+   */
+  Drupal.behaviors.fixGallery = {
+    attach: function (context) {
+      $('.media-gallery img').height($('.media-gallery img').attr('height'));
+    }
+  };
 
 }(jQuery, Drupal, drupalSettings));
 
