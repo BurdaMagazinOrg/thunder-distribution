cc9acff29911b230ec44e1221f9ebef555cabc8a
Christian Fritsch <chr.fritsch@gmx.net>
2016-10-06 17:02:59 +0200

    Remove die-on-fail

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -4,4 +4,4 @@
 cd ${TEST_DIR}/docroot
 
 # execute Drupal tests
-php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --die-on-fail --verbose --color --url http://localhost:8080 Thunder
+php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 Thunder
