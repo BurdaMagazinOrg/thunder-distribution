57acc7f07d39781618b0929d8e7b4d97156d3a0e
Mladen Todorovic <mladen.todorovic@burda.com>
2016-10-14 16:37:39 +0200

    Added Meta Information Tests

---
+++ tests/src/FunctionalJavascript/MetaInformationTest.php
@@ -0,0 +1,333 @@
+<?php
+
+namespace Drupal\Tests\thunder\FunctionalJavascript;
+
+/**
+ * Testing of Meta Information.
+ *
+ * @group Thunder
+ *
+ * @package Drupal\Tests\thunder\FunctionalJavascript
+ */
+class MetaInformationTest extends ThunderJavascriptTestBase {
+
+  use ThunderMetaTagTrait;
+
+  /**
+   * Default user login role used for testing.
+   *
+   * @var string
+   */
+  protected static $defaultUserRole = 'administrator';
+
+  /**
+   * Meta tag configuration that will be set for Global meta tags.
+   *
+   * @var array
+   */
+  protected static $globalMetaTags = [
+    'basic:title' => 'Global Title',
+    'basic:keywords' => 'Thunder,CMS,Burda',
+    'basic:abstract' => '[random]',
+    'basic:description' => '[random]',
+  ];
+
+  /**
+   * Meta tag configuration that will be set for Content meta tags.
+   *
+   * @var array
+   */
+  protected static $contentMetaTags = [
+    'basic:title' => '[node:title]',
+    'basic:abstract' => '[random]',
+  ];
+
+  /**
+   * Meta tag configuration that will be set for Content->Article meta tags.
+   *
+   * @var array
+   */
+  protected static $articleMetaTags = [
+    'basic:title' => 'Test [node:field_teaser_text]',
+    'basic:description' => '[random]',
+    'advanced:robots' => 'index, follow, noydir',
+    'advanced:referrer' => 'no-referrer-when-downgrade',
+  ];
+
+  /**
+   * Custom meta tag configuration that will be set for Article meta tags.
+   *
+   * @var array
+   */
+  protected static $customMetaTags = [
+    'basic:title' => 'Custom [node:field_teaser_text]',
+    'basic:description' => '[random]',
+    'advanced:robots' => 'follow',
+    'advanced:referrer' => 'no-referrer',
+  ];
+
+  /**
+   * List of Tokens that will be replaced with values.
+   *
+   * @var array
+   */
+  protected static $tokens = [
+    '[node:field_seo_title]' => 'Test SEO Title',
+    '[node:field_teaser_text]' => 'Test Teaser Text',
+    '[node:title]' => 'Test Note Title',
+  ];
+
+  /**
+   * Set meta tag configuration for administration url.
+   *
+   * @param string $pageUrl
+   *   Url to page where configuration should be set.
+   * @param array $configuration
+   *   List of configuration what will be set for meta tag.
+   */
+  protected function setMetaTagConfigurationForUrl($pageUrl, $configuration) {
+    $this->drupalGet($pageUrl);
+
+    $page = $this->getSession()->getPage();
+    $this->expandAllTabs();
+    $this->setFieldValues($page, $this->generateMetaTagFieldValues($configuration));
+
+    $this->scrollElementInView('[name="op"]');
+    $page->find('xpath', '//input[@name="op"]')->click();
+  }
+
+  /**
+   * Create simple article for meta tag testing.
+   *
+   * @param array $fieldValues
+   *   Custom meta tag configuration for article.
+   */
+  protected function createArticleWithFields(array $fieldValues = NULL) {
+    $this->drupalGet('node/add/article');
+
+    $page = $this->getSession()->getPage();
+
+    $page->selectFieldOption('field_channel', 1);
+    $page->fillField('title[0][value]', static::$tokens['[node:title]']);
+    $page->fillField('field_seo_title[0][value]', static::$tokens['[node:field_seo_title]']);
+    $page->fillField('field_teaser_text[0][value]', static::$tokens['[node:field_teaser_text]']);
+
+    if (isset($fieldValues)) {
+      $this->expandAllTabs();
+      $this->setFieldValues($page, $fieldValues);
+    }
+
+    $this->scrollElementInView('[data-drupal-selector="edit-save"]');
+    $page->find('xpath', '//ul[@data-drupal-selector="edit-save"]/li[1]/input')
+      ->click();
+  }
+
+  /**
+   * Test Meta Tag default configuration and custom configuration for article.
+   *
+   * TODO:
+   *   - check default preselection in from on article create.
+   */
+  public function testArticleMetaTags() {
+    $globalConfigs = $this->generateMetaTagConfiguration([static::$globalMetaTags]);
+    $contentConfigs = $this->generateMetaTagConfiguration([static::$contentMetaTags]);
+    $articleConfigs = $this->generateMetaTagConfiguration([static::$articleMetaTags]);
+    $customConfigs = $this->generateMetaTagConfiguration([static::$customMetaTags]);
+
+    // Generate check configuration for default configuration.
+    $checkArticleConfigs = $this->generateMetaTagConfiguration([
+      $globalConfigs,
+      $contentConfigs,
+      $articleConfigs,
+    ]);
+    $checkArticleMetaTags = $this->replaceTokens($checkArticleConfigs, static::$tokens);
+
+    // Generate check configuration for custom configuration.
+    $checkCustomConfigs = $this->generateMetaTagConfiguration([
+      $checkArticleConfigs,
+      $customConfigs,
+    ]);
+    $checkCustomMetaTags = $this->replaceTokens($checkCustomConfigs, static::$tokens);
+
+    // Edit Global configuration.
+    $this->setMetaTagConfigurationForUrl('admin/config/search/metatag/global', $globalConfigs);
+
+    // Edit Content configuration.
+    $this->setMetaTagConfigurationForUrl('admin/config/search/metatag/node', $contentConfigs);
+
+    // Edit Article configuration.
+    $this->setMetaTagConfigurationForUrl('admin/config/search/metatag/node__article', $articleConfigs);
+
+    // Create Article with default meta tags and check it.
+    $this->createArticleWithFields();
+    $this->checkMetaTags($checkArticleMetaTags);
+
+    // Create Article with custom meta tags and check it.
+    $this->createArticleWithFields($this->generateMetaTagFieldValues($checkCustomConfigs, 'field_meta_tags[0]'));
+    $this->checkMetaTags($checkCustomMetaTags);
+  }
+
+  /**
+   * Test Scheduling of Article.
+   */
+  public function testArticleScheduling() {
+    $articleId = 10;
+
+    // Create article with published 2 days ago, unpublish tomorrow.
+    $startTimestamp = strtotime('-2 days');
+    $endTimestamp = strtotime('+1 day');
+
+    $fieldValues = [
+      'publish_on[0][value][date]' => date('Y-m-d', $startTimestamp),
+      'publish_on[0][value][time]' => date('H:i:s', $startTimestamp),
+      'unpublish_on[0][value][date]' => date('Y-m-d', $endTimestamp),
+      'unpublish_on[0][value][time]' => date('H:i:s', $endTimestamp),
+    ];
+
+    $this->createArticleWithFields($fieldValues);
+
+    // Check that Article is unpublished.
+    $this->drupalGet('node/' . $articleId);
+    $this->assertSession()
+      ->elementExists('xpath', '//div[@class="content"]/article[contains(@class, "node--unpublished")]');
+
+    $this->runCron();
+
+    // Check that Article is published.
+    $this->drupalGet('node/' . $articleId);
+    $this->assertSession()
+      ->elementNotExists('xpath', '//div[@class="content"]/article[contains(@class, "node--unpublished")]');
+
+    // Check that Article is published.
+    $this->drupalGet('node/' . $articleId . '/edit');
+    $page = $this->getSession()->getPage();
+
+    // Edit article and set un-publish date same as publish date.
+    $unPublishDiffSeconds = 5;
+    $unPublishTimestamp = strtotime("+{$unPublishDiffSeconds} seconds");
+    $unPublishFieldValues = [
+      'unpublish_on[0][value][date]' => date('Y-m-d', $unPublishTimestamp),
+      'unpublish_on[0][value][time]' => date('H:i:s', $unPublishTimestamp),
+    ];
+
+    $this->expandAllTabs();
+    $this->setFieldValues($page, $unPublishFieldValues);
+
+    $this->scrollElementInView('[data-drupal-selector="edit-save"]');
+    $page->find('xpath', '//ul[@data-drupal-selector="edit-save"]/li[1]/input')
+      ->click();
+
+    // Check that Article is published.
+    $this->drupalGet('node/' . $articleId);
+    $this->assertSession()
+      ->elementNotExists('xpath', '//div[@class="content"]/article[contains(@class, "node--unpublished")]');
+
+    // Wait sufficient time before cron is executed.
+    sleep($unPublishDiffSeconds + 2);
+
+    $this->runCron();
+
+    // Check that Article is unpublished.
+    $this->drupalGet('node/' . $articleId);
+    $this->assertSession()
+      ->elementExists('xpath', '//div[@class="content"]/article[contains(@class, "node--unpublished")]');
+  }
+
+  /**
+   * Get SiteMap dom elements by XPath.
+   *
+   * @param string $content
+   *   XML string content of Site Map.
+   * @param string $xpathQuery
+   *   XPath to fetch elements from Site Map.
+   *
+   * @return \DOMNodeList
+   *   Returns list of elements matching provided XPath.
+   */
+  public function getSiteMapDomElements($content, $xpathQuery) {
+    $domDoc = new \DOMDocument();
+    $domDoc->loadXML($content);
+
+    $xpath = new \DOMXpath($domDoc);
+    $xpath->registerNamespace('sm', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+    $elements = $xpath->query($xpathQuery);
+
+    return $elements;
+  }
+
+  /**
+   * Test Site Map for Article.
+   */
+  public function testSiteMap() {
+    $articleId = 10;
+    $articleUrl = 'test-sitemap-seo-title';
+
+    $customFields = [
+      'field_seo_title[0][value]' => $articleUrl,
+    ];
+
+    $this->createArticleWithFields($customFields);
+
+    $this->drupalGet('node/' . $articleId . '/edit');
+    $page = $this->getSession()->getPage();
+
+    $this->scrollElementInView('[data-drupal-selector="edit-save"]');
+    $page->find('xpath', '//ul[@data-drupal-selector="edit-save"]/li[2]/button')
+      ->click();
+    $page->find('xpath', '//ul[@data-drupal-selector="edit-save"]/li[3]/input')
+      ->click();
+
+    $this->runCron();
+    $this->drupalGet('sitemap.xml');
+
+    $content = $this->getSession()->getPage()->getContent();
+    $domElements = $this->getSiteMapDomElements($content, '//sm:loc[contains(text(),"/' . $articleUrl . '")]/parent::sm:url/sm:priority');
+    $this->assertEquals(1, $domElements->length);
+    $this->assertEquals('0.5', $domElements->item(0)->nodeValue);
+
+    // After sitemap.xml -> we have to open page without setting cookie before.
+    $this->getSession()->visit($this->buildUrl('node/' . $articleId . '/edit'));
+    $page = $this->getSession()->getPage();
+
+    $this->expandAllTabs();
+    $this->setFieldValues($page, [
+      'simple_sitemap_priority' => '0.9',
+    ]);
+
+    $this->scrollElementInView('[data-drupal-selector="edit-save"]');
+    $page->find('xpath', '//ul[@data-drupal-selector="edit-save"]/li[1]/input')
+      ->click();
+
+    $this->runCron();
+    $this->drupalGet('sitemap.xml');
+
+    $content = $this->getSession()->getPage()->getContent();
+    $domElements = $this->getSiteMapDomElements($content, '//sm:loc[contains(text(),"/' . $articleUrl . '")]/parent::sm:url/sm:priority', '0.9');
+    $this->assertEquals(1, $domElements->length);
+    $this->assertEquals('0.9', $domElements->item(0)->nodeValue);
+
+    // After sitemap.xml -> we have to open page without setting cookie before.
+    $this->getSession()->visit($this->buildUrl('node/' . $articleId . '/edit'));
+    $page = $this->getSession()->getPage();
+
+    $this->expandAllTabs();
+    $this->scrollElementInView('[name="simple_sitemap_index_content"]');
+    $page->find('css', '[name="simple_sitemap_index_content"]')->click();
+
+    $this->scrollElementInView('[data-drupal-selector="edit-save"]');
+    $page->find('xpath', '//ul[@data-drupal-selector="edit-save"]/li[1]/input')
+      ->click();
+
+    $this->runCron();
+    $this->drupalGet('sitemap.xml');
+
+    $content = $this->getSession()->getPage()->getContent();
+    $domElements = $this->getSiteMapDomElements($content, '//sm:loc[contains(text(),"/' . $articleUrl . '")]');
+
+    $this->assertEquals(0, $domElements->length);
+
+    $this->getSession()->visit($this->buildUrl('node/' . $articleId . '/edit'));
+  }
+
+}
