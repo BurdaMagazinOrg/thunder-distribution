d15099d153bf0b1dcb93ffbd988ad9327fa7ca7b
Daniel Bosen <daniel.bosen@burda.com>
2016-10-18 19:10:23 +0200

    nicer code

--- scripts/travis/test-sourcecode.sh
+++ scripts/travis/test-sourcecode.sh
@@ -7,6 +7,12 @@ phpcs --standard=Drupal --report=summary -p .
 phpcs --standard=DrupalPractice --report=summary -p .
 
 # JS ESLint checking
-rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && set -x && source ~/.nvm/nvm.sh  && set +x && nvm install 4
+rm -rf ~/.nvm
+git clone https://github.com/creationix/nvm.git ~/.nvm
+(cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
+set -x
+source ~/.nvm/nvm.sh
+set +x
+nvm install 4
 npm install -g eslint
 eslint .
