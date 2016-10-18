e80f460b48003cac29354892aac48f89ddc5b133
Daniel Bosen <daniel.bosen@burda.com>
2016-10-18 19:02:55 +0200

    try to hide source error output

--- scripts/travis/test-sourcecode.sh
+++ scripts/travis/test-sourcecode.sh
@@ -7,6 +7,6 @@ phpcs --standard=Drupal --report=summary -p .
 phpcs --standard=DrupalPractice --report=summary -p .
 
 # JS ESLint checking
-rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh >/dev/null && nvm install 4
+rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh >/dev/null 2>/dev/null && nvm install 4
 npm install -g eslint
 eslint .
