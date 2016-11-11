7fe1bc5d0478a15092f6e71c6a299fc63c68b63f
Mladen Todorovic <mtodor@gmail.com>
2016-11-11 08:44:04 +0100

    Fixed Travis-CI code check script and code style fix (#148)

--- scripts/travis/test-source-code.sh
+++ scripts/travis/test-source-code.sh
@@ -6,8 +6,8 @@ composer global require drupal/coder
 
 # run phpcs
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
-phpcs --standard=Drupal --report=summary -p .
-phpcs --standard=DrupalPractice --report=summary -p .
+phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --report=summary -p .
+phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme --report=summary -p .
 
 # JS ESLint checking
 mv ~/.nvm ~/.nvm-backup
