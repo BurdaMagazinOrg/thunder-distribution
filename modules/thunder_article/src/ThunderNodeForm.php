499d8da2a6e350a26a2983d5fc0ea334c514833f
Christian Fritsch <chr.fritsch@gmx.net>
2017-01-30 17:21:27 +0100

    Never publish new nodes with continue button

--- modules/thunder_article/src/ThunderNodeForm.php
+++ modules/thunder_article/src/ThunderNodeForm.php
@@ -21,8 +21,10 @@ class ThunderNodeForm extends NodeForm {
       $element['save_continue']['#value'] = t('Save and continue');
       $element['save_continue']['#weight'] = min($element['publish']['#weight'], $element['unpublish']['#weight']) - 1;
 
+      $node = $this->entity;
+
       // If unpublish comes before publish, then we should also not publish.
-      if ($element['unpublish']['#weight'] < $element['publish']['#weight']) {
+      if ($node->isNew() || $element['unpublish']['#weight'] < $element['publish']['#weight']) {
         $element['save_continue']['#published_status'] = FALSE;
       }
     }
