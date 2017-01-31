35687c4bc6b6c1d7ed28cf2714c19fe1613a6a5d
Christian Fritsch <chr.fritsch@gmx.net>
2017-01-31 14:16:26 +0100

    Add modules to composer file, add install checkbox

---
+++ src/Plugin/Thunder/OptionalModule/AMP.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\thunder\Plugin\Thunder\OptionalModule;
+
+/**
+ * AMP.
+ *
+ * @ThunderOptionalModule(
+ *   id = "thunder_amp",
+ *   label = @Translation("AMP"),
+ *   description = @Translation("Whenever a user reaches the end of an article, the Infinite Theme loads further relevant content."),
+ *   type = "theme",
+ * )
+ */
+class AMP extends AbstractOptionalModule {}
