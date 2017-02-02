b4f4bdee89e774ad4ecfdb8bbd419d18180c6104
Christian Fritsch <chr.fritsch@gmx.net>
2017-02-02 17:21:40 +0100

    Add thunder_demo as dependency

--- tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
@@ -21,6 +21,19 @@ use Symfony\Component\HttpFoundation\Request;
 abstract class ThunderJavascriptTestBase extends JavascriptTestBase {
 
   /**
+   * Modules to enable.
+   *
+   * The test runner will merge the $modules lists from this class, the class
+   * it extends, and so on up the class hierarchy. It is not necessary to
+   * include modules in your list that a parent class has already declared.
+   *
+   * @var string[]
+   *
+   * @see \Drupal\Tests\BrowserTestBase::installDrupal()
+   */
+  protected static $modules = ['thunder_demo'];
+
+  /**
    * The profile to install as a basis for testing.
    *
    * @var string
