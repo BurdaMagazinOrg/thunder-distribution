5518980296d6e85dfc8eed387defe64cf3e21380
Christian Fritsch <chr.fritsch@gmx.net>
2017-02-03 09:25:59 +0100

    Move ThunderBaseTest to BTB

--- tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
@@ -34,19 +34,6 @@ abstract class ThunderJavascriptTestBase extends JavascriptTestBase {
   protected static $modules = ['thunder_demo'];
 
   /**
-   * Modules to enable.
-   *
-   * The test runner will merge the $modules lists from this class, the class
-   * it extends, and so on up the class hierarchy. It is not necessary to
-   * include modules in your list that a parent class has already declared.
-   *
-   * @var string[]
-   *
-   * @see \Drupal\Tests\BrowserTestBase::installDrupal()
-   */
-  protected static $modules = ['thunder_demo'];
-
-  /**
    * The profile to install as a basis for testing.
    *
    * @var string
@@ -98,7 +85,7 @@ abstract class ThunderJavascriptTestBase extends JavascriptTestBase {
       $this->drupalLogout();
     }
 
-    $this->drupalGet('user/login');
+    $this->drupalGet('user');
     $this->submitForm(array(
       'name' => $account->getUsername(),
       'pass' => $account->passRaw,
