501878e38b20a86eaac49f5078c03710eb90b7b5
Christian Fritsch <chr.fritsch@gmx.net>
2017-02-03 15:08:11 +0100

    Use php 7.1

--- scripts/travis/02-before-install.sh
+++ scripts/travis/02-before-install.sh
@@ -40,10 +40,7 @@ yes '' | pecl install imagick
 # acknowledge all prompts.
 if [[ $TRAVIS_PHP_VERSION = '5.6' ]] ; then
   yes '' | pecl install yaml;
-  yes '' | pecl install apcu-4.0.8;
- echo "apc.enabled=1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
- echo "apc.enable_cli=1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-elif [[ $TRAVIS_PHP_VERSION = '7.0' ]] ; then
+elif [[ $TRAVIS_PHP_VERSION = '7.1' ]] ; then
  yes '' | pecl install yaml-2.0.0;
  yes '' | pecl install apcu;
 fi;
