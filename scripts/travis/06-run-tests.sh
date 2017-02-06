3bef653ced9e7a3a3656c0334bce9d8165f8830e
Christian Fritsch <chr.fritsch@gmx.net>
2017-02-06 13:28:03 +0100

    Move Installer tests to update tests

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -5,3 +5,7 @@ cd ${TEST_DIR}/docroot
 
 # execute Drupal tests
 thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 Thunder
+
+if [[ ${TEST_UPDATE} == "true" ]]; then
+    thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 ThunderInstaller
+fi
