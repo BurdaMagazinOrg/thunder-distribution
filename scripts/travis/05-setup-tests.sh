ea6856df5ea731127e5932e9f5803ac26ce21d6d
Christian Fritsch <chr.fritsch@gmx.net>
2017-02-06 15:34:24 +0100

    Feature/speed up tests (#228)
    * Use own database
    
    * Add thunder_demo as dependency
    
    * Enable apc
    
    * Try apu
    
    * Use php 7.1

--- scripts/travis/05-setup-tests.sh
+++ scripts/travis/05-setup-tests.sh
@@ -5,9 +5,9 @@ cd ${TEST_DIR}/docroot
 
 # require Selenium2 Driver
 if [[ ${INSTALL_METHOD} == "drush_make" ]]; then
-    composer require "behat/mink-selenium2-driver" --no-progress --working-dir ${TEST_DIR}/docroot
+    composer require "behat/mink-selenium2-driver" "behat/mink-goutte-driver" --no-progress --working-dir ${TEST_DIR}/docroot
 elif [[ ${INSTALL_METHOD} == "composer" ]]; then
-    composer require "behat/mink-selenium2-driver" --no-progress --working-dir ${TEST_DIR}
+    composer require "behat/mink-selenium2-driver" "behat/mink-goutte-driver" --no-progress --working-dir ${TEST_DIR}
 fi
 
 # Final cache rebuild, to make sure every code change is respected
