8b2910447871eb58d6e4902b7417730e6aedd95a
Mladen Todorovic <mladen.todorovic@burda.com>
2017-02-08 10:30:02 +0100

    Adjust documentation and additional code cleanup

--- scripts/travis/05-setup-tests.sh
+++ scripts/travis/05-setup-tests.sh
@@ -16,7 +16,12 @@ drush cr
 # Run the webserver
 drush runserver --default-server=builtin 8080 &>/dev/null &
 
+# Run Sauce Labs connector if Sauce Labs is enabled
+if [[ ${SAUCE_LABS_ENABLED} == "true" ]]; then
+    travis_start_sauce_connect
+fi
+
 # Run Selenium2 Server
 bash -e /etc/init.d/xvfb start
 sleep 3
-java -jar -Dwebdriver.chrome.driver="${SELENIUM_PATH}/chromedriver-$CHROME_DRIVER_VERSION" "${SELENIUM_PATH}/selenium-server-standalone-3.0.1.jar" > /dev/null 2>&1 &
+java -jar -Dwebdriver.chrome.driver="${SELENIUM_PATH}/chromedriver-$CHROME_DRIVER_VERSION" "${SELENIUM_PATH}/selenium-server-standalone-$SELENIUM_VERSION.jar" > /dev/null 2>&1 &
