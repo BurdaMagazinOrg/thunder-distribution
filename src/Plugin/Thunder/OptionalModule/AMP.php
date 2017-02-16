0b7aeb12bc7bad4d7dcccc6696286ea68763d672
goldquest <tjwelde@gmail.com>
2017-02-16 17:08:04 +0100

    change AMP description for the optional module installation

--- src/Plugin/Thunder/OptionalModule/AMP.php
+++ src/Plugin/Thunder/OptionalModule/AMP.php
@@ -8,7 +8,7 @@ namespace Drupal\thunder\Plugin\Thunder\OptionalModule;
  * @ThunderOptionalModule(
  *   id = "thunder_amp",
  *   label = @Translation("AMP"),
- *   description = @Translation("Whenever a user reaches the end of an article, the Infinite Theme loads further relevant content."),
+ *   description = @Translation("The Google AMP project strives for better performance, especially on mobile devices."),
  *   type = "theme",
  * )
  */
