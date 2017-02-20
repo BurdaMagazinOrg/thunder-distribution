352d30310ea67a2ddd359a4ba8b223f8ac91d470
Daniel Bosen <daniel.bosen@burda.com>
2017-02-20 11:30:28 +0100

    Feature/verbose phpcs (#247)

--- scripts/travis/03-test-source-code.sh
+++ scripts/travis/03-test-source-code.sh
@@ -1,7 +1,7 @@
 # run phpcs
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
-phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --report=summary -p .
-phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme --report=summary -p .
+phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme -p .
+phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme -p .
 
 # JS ESLint checking
 mv ~/.nvm ~/.nvm-backup
