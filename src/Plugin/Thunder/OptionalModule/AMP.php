9c8a71cb45594d7fec4848f744a841ac86e23cb6
goldquest <tjwelde@gmail.com>
2017-03-01 12:06:51 +0100

    AMP: Provide disclaimer for non-composer installs.

--- src/Plugin/Thunder/OptionalModule/AMP.php
+++ src/Plugin/Thunder/OptionalModule/AMP.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\thunder\Plugin\Thunder\OptionalModule;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * AMP.
@@ -12,4 +13,30 @@ namespace Drupal\thunder\Plugin\Thunder\OptionalModule;
  *   type = "theme",
  * )
  */
-class AMP extends AbstractOptionalModule {}
+class AMP extends AbstractOptionalModule {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form = parent::buildForm($form, $form_state);
+
+    if (!class_exists('\Lullabot\AMP\AMP')) {
+      $form['thunder_amp']['library_info'] = [
+        '#type' => 'item',
+        '#description' => $this->t("ATTENTION: Due to licensing issues,
+        you have to download the needed library manually, before activating this module.
+        With composer installed execute the command `composer require lullabot/amp`
+        in the docroot of your installation."),
+      ];
+    }
+    else {
+      unset($form['thunder_amp']);
+    }
+
+    return $form;
+
+  }
+
+}
