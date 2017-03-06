e2e3120320711420bd4ea552dd8615ae297430a4
Christian Fritsch <chr.fritsch@gmx.net>
2017-03-06 13:30:14 +0100

    Prefix all dependencies

--- scripts/travis/test-source-code.sh
+++ scripts/travis/test-source-code.sh
@@ -4,7 +4,7 @@ phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 # Ignore check of .md files, because they should be able to contain more then 80 characters per line.
 phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=*.md -p .
 # Ignore *.yml file because the enforced usage of prefixed dependencies eems to be not working.
-phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme --ignore=*.yml -p .
+phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme --ignore=thunder.info.yml -p .
 
 # JS ESLint checking
 mv ~/.nvm ~/.nvm-backup
