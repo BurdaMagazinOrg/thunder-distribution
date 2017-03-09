191c2842ed2e875d3e1617fb2fac180b36b04430
Daniel Bosen <daniel.bosen@burda.com>
2017-03-09 18:03:50 +0100

    remove the hole thing

--- scripts/travis/02-before-install.sh
+++ scripts/travis/02-before-install.sh
@@ -52,10 +52,6 @@ if [[ $TRAVIS_PHP_VERSION = '5.6' ]] ; then
   yes '' | pecl install yaml;
 elif [[ $TRAVIS_PHP_VERSION = '7.1' ]] ; then
  yes '' | pecl install yaml-2.0.0;
-
- if ! pecl list | grep apcu >/dev/null 2>&1 ; then
-   yes '' | pecl install apcu;
- fi;
 fi;
 
 # Set MySQL Options
