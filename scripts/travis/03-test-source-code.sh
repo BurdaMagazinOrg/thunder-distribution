fae1ebbcee7e8583822d105ab8ebefba22ec75d5
Christian Fritsch <chr.fritsch@gmx.net>
2017-03-17 10:02:59 +0100

    Remove nvm and custom npm

--- scripts/travis/03-test-source-code.sh
+++ scripts/travis/03-test-source-code.sh
@@ -7,14 +7,5 @@ phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme -
 phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme --ignore=thunder.info.yml -p .
 
 # JS ESLint checking
-mv ~/.nvm ~/.nvm-backup
-git clone https://github.com/creationix/nvm.git ~/.nvm
-(cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
-set -x
-source ~/.nvm/nvm.sh
-set +x
-nvm install 4
 npm install -g eslint
 eslint .
-rm -rf ~/.nvm
-mv ~/.nvm-backup ~/.nvm
