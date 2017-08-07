e3f47ef2133d28cdf975e08a22ebb970b8801437
Mladen Todorovic <mladen.todorovic@burda.com>
2017-08-07 18:07:43 +0200

    Additional check if element has function to get scrollable parent element

--- tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
@@ -282,7 +282,7 @@ abstract class ThunderJavascriptTestBase extends JavascriptTestBase {
         var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
         var element = jQuery(\'' . addcslashes($cssSelector, '\'') . '\');
         var scrollTop = element.offset().top - (viewPortHeight/2);
-        var scrollableParent = element.scrollParent();
+        var scrollableParent = jQuery.isFunction(element.scrollParent) ? element.scrollParent() : [];
         if (scrollableParent.length > 0 && scrollableParent[0] !== document) { scrollableParent[0].scrollTop = scrollTop } else { window.scroll(0, scrollTop); };
       ');
   }
