fc9d899c45aa35742d1d87fe4f9e91217a311c7c
Mladen Todorovic <mladen.todorovic@burda.com>
2017-08-11 18:04:03 +0200

    Remove InDev flag for test group

--- tests/src/Functional/ModuleUninstallTest.php
+++ tests/src/Functional/ModuleUninstallTest.php
@@ -9,7 +9,7 @@ use Drupal\thunder\ThunderBaseTest;
  *
  * @package Drupal\Tests\thunder\Kernel
  *
- * @group ThunderConfig_InDev
+ * @group ThunderConfig
  */
 class ModuleUninstallTest extends ThunderBaseTest {
 
