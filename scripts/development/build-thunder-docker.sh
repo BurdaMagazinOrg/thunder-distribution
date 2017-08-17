cfff7d2e73004b52e80c6bd79de7aa9deb9fbdf7
Daniel Bosen <daniel.bosen@burda.com>
2017-08-17 16:36:22 +0200

    introduce docker-sync

--- scripts/development/build-thunder-docker.sh
+++ scripts/development/build-thunder-docker.sh
@@ -1,36 +1,42 @@
 #!/usr/bin/env bash
 
-DISTRIBUTION_REPOSITORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
-
-THUNDER_DIRECTORY="$DISTRIBUTION_REPOSITORY/../thunder"
-
 EXPOSED_HTTP_PORT=80
 
-DOCKER_EXEC_PHP="docker-compose exec --user 82 php"
+DISTRIBUTION_REPOSITORY=$( cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.." && pwd )
+
+DISTRIBUTION_REPOSITORY_SYNC_KEY=${DISTRIBUTION_REPOSITORY//\//-}
+DISTRIBUTION_REPOSITORY_SYNC_KEY=${DISTRIBUTION_REPOSITORY_SYNC_KEY/-/}
+
+THUNDER_DIRECTORY=$( cd "${DISTRIBUTION_REPOSITORY}/../thunder" && pwd )
 
 if [ $1 ] ; then
-  THUNDER_DIRECTORY="$1"
+  mkdir -p ${1}
+  THUNDER_DIRECTORY=$( cd ${1} && pwd )
 fi
 
 if [ $2 ] ; then
   EXPOSED_HTTP_PORT="$2"
 fi
 
+THUNDER_DIRECTORY_SYNC_KEY=${THUNDER_DIRECTORY//\//-}
+THUNDER_DIRECTORY_SYNC_KEY=${THUNDER_DIRECTORY_SYNC_KEY/-/}
+
+DOCKER_EXEC_PHP="docker-compose exec --user 82 php"
+
 composer create-project burdamagazinorg/thunder-project:2.x ${THUNDER_DIRECTORY} --stability dev --no-interaction --no-install
 
-sed -e "s|EXPOSED_HTTP_PORT|${EXPOSED_HTTP_PORT}|g" -e "s|THUNDER_DIRECTORY|${THUNDER_DIRECTORY}|g" -e "s|DISTRIBUTION_REPOSITORY|${DISTRIBUTION_REPOSITORY}|g" ${DISTRIBUTION_REPOSITORY}/scripts/development/docker-compose.template.yml > ${THUNDER_DIRECTORY}/docker-compose.yml
+sed -e "s|THUNDER_DIRECTORY|${THUNDER_DIRECTORY}|g" -e "s|DISTRIBUTION_REPOSITORY|${DISTRIBUTION_REPOSITORY}|g" -e "s|THUNDER_SYNC_DIRECTORY|${THUNDER_DIRECTORY_SYNC_KEY}|g" -e "s|DISTRIBUTION_SYNC_REPOSITORY|${DISTRIBUTION_REPOSITORY_SYNC_KEY}|g" -e "s|EXPOSED_HTTP_PORT|${EXPOSED_HTTP_PORT}|g" ${DISTRIBUTION_REPOSITORY}/scripts/development/docker-compose.template.yml > ${THUNDER_DIRECTORY}/docker-compose.yml
+sed -e "s|THUNDER_DIRECTORY|${THUNDER_DIRECTORY}|g" -e "s|DISTRIBUTION_REPOSITORY|${DISTRIBUTION_REPOSITORY}|g" -e "s|THUNDER_SYNC_DIRECTORY|${THUNDER_DIRECTORY_SYNC_KEY}|g" -e "s|DISTRIBUTION_SYNC_REPOSITORY|${DISTRIBUTION_REPOSITORY_SYNC_KEY}|g" ${DISTRIBUTION_REPOSITORY}/scripts/development/docker-sync-template.yml > ${THUNDER_DIRECTORY}/docker-sync.yml
 
 cd ${THUNDER_DIRECTORY}
 
 composer config repositories.thunder path ${DISTRIBUTION_REPOSITORY}
 composer require "burdamagazinorg/thunder:*" "phpunit/phpunit:~4.8" "behat/mink-selenium2-driver" "behat/mink-goutte-driver" "mikey179/vfsStream" "burdamagazinorg/thunder-dev-tools:*" "burdamagazinorg/robo:*" --no-progress
 
-# The distribution directory gets mounted and cached on docker-compose up
-rm docroot/profiles/contrib/thunder
-
 echo "<?php use Thunder\Robo\RoboFileBase; class RoboFile extends RoboFileBase {}" > RoboFile.php
 
-docker-compose down -v
-docker-compose up -d
+rm docroot/profiles/contrib/thunder
 
-#$DOCKER_EXEC_PHP drush -r docroot si thunder --account-name=admin --account-pass=admin --db-url=mysql://drupal:drupal@mariadb/drupal -y
+docker-compose down -v
+docker-sync start
+docker-compose up -d
