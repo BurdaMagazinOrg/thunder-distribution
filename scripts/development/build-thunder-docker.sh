276f4271f424ecf19bf2f1fde21287eb336d707a
Daniel Bosen <daniel.bosen@burda.com>
2017-08-17 13:00:40 +0200

    mount differntly

--- scripts/development/build-thunder-docker.sh
+++ scripts/development/build-thunder-docker.sh
@@ -16,21 +16,21 @@ if [ $2 ] ; then
   EXPOSED_HTTP_PORT="$2"
 fi
 
-composer create-project burdamagazinorg/thunder-project:2.x ${THUNDER_DIRECTORY} --stability dev --no-interaction
+composer create-project burdamagazinorg/thunder-project:2.x ${THUNDER_DIRECTORY} --stability dev --no-interaction --no-install
 
 sed -e "s|EXPOSED_HTTP_PORT|${EXPOSED_HTTP_PORT}|g" -e "s|THUNDER_DIRECTORY|${THUNDER_DIRECTORY}|g" -e "s|DISTRIBUTION_REPOSITORY|${DISTRIBUTION_REPOSITORY}|g" ${DISTRIBUTION_REPOSITORY}/scripts/development/docker-compose.template.yml > ${THUNDER_DIRECTORY}/docker-compose.yml
 
 cd ${THUNDER_DIRECTORY}
 
-#composer config repositories.thunder path ${DISTRIBUTION_REPOSITORY}
-composer require "phpunit/phpunit:~4.8" "behat/mink-selenium2-driver" "behat/mink-goutte-driver" "mikey179/vfsStream" "burdamagazinorg/thunder-dev-tools:*" "burdamagazinorg/robo:*" --no-progress
+composer config repositories.thunder path ${DISTRIBUTION_REPOSITORY}
+composer require "burdamagazinorg/thunder:*" "phpunit/phpunit:~4.8" "behat/mink-selenium2-driver" "behat/mink-goutte-driver" "mikey179/vfsStream" "burdamagazinorg/thunder-dev-tools:*" "burdamagazinorg/robo:*" --no-progress
+
+# The distribution directory gets mounted and cached on docker-compose up
+rm docroot/profiles/contrib/thunder
 
 echo "<?php use Thunder\Robo\RoboFileBase; class RoboFile extends RoboFileBase {}" > RoboFile.php
 
 docker-compose down -v
 docker-compose up -d
-
-#$DOCKER_EXEC_PHP rm docroot/profiles/contrib/thunder
-#$DOCKER_EXEC_PHP ln -s ${DISTRIBUTION_REPOSITORY} docroot/profiles/contrib/thunder
 
 #$DOCKER_EXEC_PHP drush -r docroot si thunder --account-name=admin --account-pass=admin --db-url=mysql://drupal:drupal@mariadb/drupal -y
