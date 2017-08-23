e5ee483bd557caf5e8bc41c5150d855988f33ac6
Daniel Bosen <daniel.bosen@burda.com>
2017-08-23 15:04:37 +0200

    do not use docker sync

--- scripts/development/build-thunder-docker.sh
+++ scripts/development/build-thunder-docker.sh
@@ -29,20 +29,15 @@ DISTRIBUTION_KEY=${DISTRIBUTION_REPOSITORY//\//}
 composer create-project burdamagazinorg/thunder-project:2.x ${PROJECT_DIRECTORY} --stability dev --no-interaction --no-install
 
 sed -e "s|PROJECT_DIRECTORY|${PROJECT_DIRECTORY}|g" -e "s|DISTRIBUTION_REPOSITORY|${DISTRIBUTION_REPOSITORY}|g" -e "s|PROJECT_KEY|${PROJECT_KEY}|g" -e "s|DISTRIBUTION_KEY|${DISTRIBUTION_KEY}|g" -e "s|PROJECT_DOCKER_NAME|${PROJECT_DOCKER_NAME}|g" ${DISTRIBUTION_REPOSITORY}/scripts/development/docker-compose.template.yml > ${PROJECT_DIRECTORY}/docker-compose.yml
-sed -e "s|PROJECT_DIRECTORY|${PROJECT_DIRECTORY}|g" -e "s|DISTRIBUTION_REPOSITORY|${DISTRIBUTION_REPOSITORY}|g" -e "s|PROJECT_KEY|${PROJECT_KEY}|g" -e "s|DISTRIBUTION_KEY|${DISTRIBUTION_KEY}|g" ${DISTRIBUTION_REPOSITORY}/scripts/development/docker-sync-template.yml > ${PROJECT_DIRECTORY}/docker-sync.yml
 
 cd ${PROJECT_DIRECTORY}
 
 composer config repositories.thunder path ${DISTRIBUTION_REPOSITORY}
 composer require "burdamagazinorg/thunder:*" "phpunit/phpunit:~4.8" "behat/mink-selenium2-driver" "behat/mink-goutte-driver" "mikey179/vfsStream" "burdamagazinorg/thunder-dev-tools:*" "burdamagazinorg/robo:*" --no-progress
 
-rm docroot/profiles/contrib/thunder
-
 echo "<?php use Thunder\Robo\RoboFileBase; class RoboFile extends RoboFileBase {}" > RoboFile.php
 
-docker-compose exec php echo -e "<?php\n\nrequire_once DRUPAL_ROOT . '/sites/default/default.settings.php';\n\$settings['hash_salt'] = '$(date | md5)';\n\$databases['default']['default'] = [\n  'database' => 'thunder',\n  'username' => 'thunder',\n  'password' => 'thunder',\n  'prefix' => '',\n  'host' => 'mariadb',\n  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',\n  'driver' => 'mysql'\n];\n" > docroot/sites/default/settings.php
+echo -e "<?php\n\nrequire_once DRUPAL_ROOT . '/sites/default/default.settings.php';\n\$settings['hash_salt'] = '$(date | md5)';\n\$databases['default']['default'] = [\n  'database' => 'thunder',\n  'username' => 'thunder',\n  'password' => 'thunder',\n  'prefix' => '',\n  'host' => 'mariadb',\n  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',\n  'driver' => 'mysql'\n];\n" > docroot/sites/default/settings.php
 
-docker-compose down -v
-docker-sync start
+docker-compose down -v --remove-orphans
 docker-compose -p ${PROJECT_DOCKER_NAME} up -d
-
