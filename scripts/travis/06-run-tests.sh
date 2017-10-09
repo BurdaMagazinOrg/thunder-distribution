199c709140c9c82347b7cd135d2bf34b21c9c424
Mladen Todorovic <mladen.todorovic@burda.com>
2017-10-09 10:13:32 +0200

    [TEST_UPDATE=true] Change to two explicit executions for simpletests

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -4,16 +4,14 @@
 cd ${TEST_DIR}/docroot
 
 # disable configuration testing for update test path
-#if [[ ${TEST_UPDATE} != "true" ]]; then
-#    thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 ThunderConfig
-#fi
+if [[ ${TEST_UPDATE} != "true" ]]; then
+    thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 ThunderConfig
+fi
 
 # execute Drupal tests
-#thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 Thunder,thunder_updater
+thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 Thunder,thunder_updater
 
 if [[ ${TEST_UPDATE} == "true" || ${TEST_INSTALLER} == "true" ]]; then
-    echo "opcache.enable=0" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-    echo "opcache.enable_cli=0" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-    phpenv rehash
-    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 ThunderInstaller
+    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 --class "Drupal\thunder\Tests\Installer\ThunderInstallerTest"
+    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 --class "Drupal\thunder\Tests\Installer\ThunderInstallerGermanTest"
 fi
