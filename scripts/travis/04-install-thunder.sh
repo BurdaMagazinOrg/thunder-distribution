01986c07386fb397f65168002420c6836615207e
Mladen Todorovic <mladen.todorovic@burda.com>
2017-10-20 18:08:29 +0200

    Adjusted git repo for testing

--- scripts/travis/04-install-thunder.sh
+++ scripts/travis/04-install-thunder.sh
@@ -38,7 +38,7 @@ drush_make_thunder() {
 
     # Get development branch of Thunder Admin theme (to use same admin theme as for composer build)
     rm -rf ${TEST_DIR}/docroot/profiles/thunder/themes/thunder_admin
-    git clone --depth 1 --branch "feature/2908887-experimental-paragraphs-widget" https://git.drupal.org/project/thunder_admin.git ${TEST_DIR}/docroot/profiles/thunder/themes/thunder_admin
+    git clone --depth 1 --single-branch --branch "feature/2908887-experimental-paragraphs-widget" https://github.com/BurdaMagazinOrg/theme-thunder-admin.git ${TEST_DIR}/docroot/profiles/thunder/themes/thunder_admin
 
     composer install --working-dir=${TEST_DIR}/docroot
 }
@@ -49,6 +49,7 @@ composer_create_thunder() {
 
     cd ${TEST_DIR}
     composer config repositories.thunder path ${THUNDER_DIST_DIR}
+    composer config repositories.thunder_admin git "https://github.com/BurdaMagazinOrg/theme-thunder-admin.git"
     composer require "burdamagazinorg/thunder:*" "phpunit/phpunit:~4.8" "drupal/thunder_admin:dev-feature/2908887-experimental-paragraphs-widget" --no-progress
 }
 
