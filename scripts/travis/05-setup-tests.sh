aa8077ab98a806c2a1550e74e695d28df4cc75f2
Christian Fritsch <chr.fritsch@gmx.net>
2017-11-13 12:50:35 +0100

    Add port

--- scripts/travis/05-setup-tests.sh
+++ scripts/travis/05-setup-tests.sh
@@ -23,3 +23,7 @@ if [[ ${SAUCE_LABS_ENABLED} == "true" ]]; then
 
     travis_start_sauce_connect
 fi
+
+docker pull selenium/standalone-chrome
+docker run -d -p 4444:4444 --add-host="docker:172.17.0.1" selenium/standalone-chrome
+docker ps -a
