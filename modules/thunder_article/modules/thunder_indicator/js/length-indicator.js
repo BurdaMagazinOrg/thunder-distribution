a03567264334063f23f9eb8335c589bc3a7bb539
Timo Welde <tjwelde@gmail.com>
2017-11-14 14:54:35 +0100

    convert ES6 zu ES5

--- modules/thunder_article/modules/thunder_indicator/js/length-indicator.js
+++ modules/thunder_article/modules/thunder_indicator/js/length-indicator.js
@@ -15,7 +15,7 @@
         }
       )
     }
-  }
+  };
 
   function Indicator($el, $context, optimin, optimax, tolerance) {
     this.$el = $el;
@@ -32,20 +32,21 @@
 
     this.scaleIndicators();
 
-    this.$el.on('input', (e) => {
-      this.setCursorAndActiveIndicator();
+    var self = this;
+    this.$el.on('input', function(e) {
+      self.setCursorAndActiveIndicator();
     });
     this.setCursorAndActiveIndicator();
   }
 
   Indicator.prototype.scaleIndicators = function() {
-    let total = this.settings.max + this.settings.min;
+    var total = this.settings.max + this.settings.min;
 
-    let width = (this.settings.min/total)*100;
+    var width = (this.settings.min/total)*100;
     this.allIndicators.eq(0).css('width', width+'%').data('pos', 0);
     // Adding +1 to make max inclusive
     this.allIndicators.eq(4).css('width', width+'%').data('pos', this.settings.max+1);
-    let last = width;
+    var last = width;
 
     width = (this.settings.optimin/total)*100;
     this.allIndicators.eq(1).css('width', (width-last)+'%').data('pos', this.settings.min);
@@ -61,18 +62,18 @@
   };
 
   Indicator.prototype.setCursorAndActiveIndicator = function() {
-    let length = this.$el.val().length;
-    let max = this.settings.max+this.settings.min;
-    let position = (length/max)*100;
+    var length = this.$el.val().length;
+    var max = this.settings.max+this.settings.min;
+    var position = (length/max)*100;
 
     position = position < 100 ? position : 100;
     this.cursor.css('left', position + '%');
 
     this.allIndicators.removeClass('active');
 
-    let coloredIndicator = this.allIndicators.eq(0);
-    for (let i=1; i<this.allIndicators.length; i++) {
-      let indicator = this.allIndicators.eq(i);
+    var coloredIndicator = this.allIndicators.eq(0);
+    for (var i=1; i<this.allIndicators.length; i++) {
+      var indicator = this.allIndicators.eq(i);
       if (length >= indicator.data('pos')) {
         coloredIndicator = indicator;
       }
