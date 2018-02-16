9a5a7c5536a9fd2cccfba8fe5d11624b9835a2c0
Alex Pott <alex.a.pott@googlemail.com>
2018-02-16 16:52:48 +0000

    Use checklist in post update

--- thunder.post_update.php
+++ thunder.post_update.php
@@ -9,5 +9,10 @@
  * Install the Configuration Selector module.
  */
 function thunder_post_update_install_config_selector() {
-  \Drupal::service('module_installer')->install(['config_selector']);
+  /** @var \Drupal\thunder_updater\Updater $thunderUpdater */
+  $thunderUpdater = \Drupal::service('thunder_updater');
+  $thunderUpdater->installModules(['thunder_post_update_install_config_selector' => 'config_selector']);
+
+  // Output logged messages to related channel of update execution.
+  return $thunderUpdater->logger()->output();
 }
