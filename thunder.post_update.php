2f87ca99bf1f079956407524f2bb91c47738c5e1
Christian Fritsch <chr.fritsch@gmx.net>
2018-02-22 14:31:48 +0100

    Add a comment

--- thunder.post_update.php
+++ thunder.post_update.php
@@ -12,6 +12,10 @@ function thunder_post_update_ensure_config_selector_installed() {
   /** @var \Drupal\thunder_updater\Updater $thunderUpdater */
   $thunderUpdater = \Drupal::service('thunder_updater');
   if (!\Drupal::moduleHandler()->moduleExists('config_selector')) {
+    // This function was renamed after Thunder 2.14, because we want to ensure
+    // that the config_selector is always enabled.
+    // thunder_post_update_install_config_selector was the former name and is
+    // still the key for the checklist.
     $thunderUpdater->installModules(['thunder_post_update_install_config_selector' => 'config_selector']);
   }
 
