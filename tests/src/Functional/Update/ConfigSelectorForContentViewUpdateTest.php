a61308b1c425a5f853a9b5c86b68e927bb0105c0
Christian Fritsch <chr.fritsch@gmx.net>
2018-03-01 15:40:36 +0100

    Finialize update path testing

--- tests/src/Functional/Update/ConfigSelectorForContentViewUpdateTest.php
+++ tests/src/Functional/Update/ConfigSelectorForContentViewUpdateTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\thunder\Functional\Update;
 
+use Drupal\Component\Serialization\Yaml;
 use Drupal\views\Entity\View;
 
 /**
@@ -24,7 +25,7 @@ class ConfigSelectorForContentViewUpdateTest extends ThunderUpdatePathTestBase {
   }
 
   /**
-   * Tests Thunder updates from 2.0 to current.
+   * Test adding config_selector to content view when nothing was changed.
    */
   public function testFreshInstallBehavior() {
     // Calling the method below performs assertions that all updates have been
@@ -32,6 +33,45 @@ class ConfigSelectorForContentViewUpdateTest extends ThunderUpdatePathTestBase {
     // schemas are correct.
     $this->runUpdates();
 
+    $this->verifyViewStates();
+
+    $content_view = View::load('content');
+    $this->assertFalse($content_view->status());
+    $content_lock_view = View::load('content_content_lock');
+    $this->assertTrue($content_lock_view->status());
+  }
+
+  /**
+   * Test adding config_selector to content view without content_lock fields.
+   */
+  public function testWithoutContentLockFields() {
+    $content_view = \Drupal::configFactory()->getEditable('views.view.content');
+    $view_config = Yaml::decode(file_get_contents(__DIR__ . '/../../../fixtures/configs/views.view.content_without_content_lock_and_config_selector.yml'));
+    $content_view->setData($view_config);
+    $content_view->save();
+
+    $content_view = View::load('content');
+    $this->assertNotNull($content_view, 'The content view exists');
+    $this->assertFalse(in_array('content_lock', $content_view->getDependencies()['module']), 'Content view is without content_lock dependency');
+    $this->assertEmpty($content_view->getThirdPartySettings('config_selector'), 'Third party settings for config_selector are not present');
+
+    // Calling the method below performs assertions that all updates have been
+    // run successfully, configuration schema is correct and that the entity
+    // schemas are correct.
+    $this->runUpdates();
+
+    $this->verifyViewStates();
+
+    $content_view = View::load('content');
+    $this->assertTrue($content_view->status());
+    $content_lock_view = View::load('content_content_lock');
+    $this->assertFalse($content_lock_view->status());
+  }
+
+  /**
+   * Verifies the settings of the content views.
+   */
+  protected function verifyViewStates() {
     $content_view = View::load('content');
     $this->assertNotNull($content_view, 'The content view exists');
     $this->assertFalse(in_array('content_lock', $content_view->getDependencies()['module']), 'Content view is without content_lock dependency');
--- thunder.post_update.php
+++ thunder.post_update.php
@@ -37,7 +37,61 @@ function thunder_post_update_add_content_lock_view() {
   $content_view->setThirdPartySetting('config_selector', 'feature', 'thunder_content_view');
   $content_view->setThirdPartySetting('config_selector', 'priority', 0);
 
-  if (!\Drupal::moduleHandler()->moduleExists('content_lock')) {
+  $view_contains_content_lock = FALSE;
+  foreach (array_keys($content_view->get('display')) as $display_name) {
+    $display = $content_view->getDisplay($display_name);
+    $options = ['fields', 'filters', 'sorts', 'relationships'];
+    foreach ($options as $option) {
+      if (!empty($display['display_options'][$option]) && is_array($display['display_options'][$option])) {
+        foreach ($display['display_options'][$option] as $key => $values) {
+          if ($values['table'] == 'content_lock') {
+            $view_contains_content_lock = TRUE;
+          }
+        }
+      }
+    }
+  }
+
+  if ($view_contains_content_lock) {
+    // Duplicate content into content_content_lock view and add third party
+    // settings.
+    $content_lock_view = $content_view->createDuplicate();
+    $content_lock_view->set('id', 'content_content_lock');
+    $content_lock_view->setThirdPartySetting('config_selector', 'feature', 'thunder_content_view');
+    $content_lock_view->setThirdPartySetting('config_selector', 'priority', 1);
+    $content_lock_view->save();
+
+    // Delete all content_lock related stuff from the content view.
+    foreach (array_keys($content_view->get('display')) as $display_name) {
+      $display = &$content_view->getDisplay($display_name);
+
+      // Delete content_lock relationships.
+      $deleted_relationships = [];
+      if (!empty($display['display_options']['relationships']) && is_array($display['display_options']['relationships'])) {
+        foreach ($display['display_options']['relationships'] as $key => $relationship) {
+          if ($relationship['table'] == 'content_lock') {
+            unset($display['display_options']['relationships'][$key]);
+            $deleted_relationships[] = $key;
+          }
+        }
+      }
+
+      // Delete content lock fields, filters and sorts and those that depend
+      // on a deleted relationship.
+      $options = ['fields', 'filters', 'sorts'];
+      foreach ($options as $option) {
+        if (!empty($display['display_options'][$option]) && is_array($display['display_options'][$option])) {
+          foreach ($display['display_options'][$option] as $key => $values) {
+            if ($values['table'] == 'content_lock' || (!empty($values['relationship']) && in_array($values['relationship'], $deleted_relationships))) {
+              unset($display['display_options'][$option][$key]);
+            }
+          }
+        }
+      }
+    }
+    $content_view->disable();
+  }
+  else {
     /** @var \Drupal\config_update\ConfigReverter $config_service */
     $config_service = \Drupal::service('config_update.config_update');
     $config_service->import('view', 'content_content_lock');
@@ -45,69 +99,6 @@ function thunder_post_update_add_content_lock_view() {
     $content_lock_view->disable();
     $content_lock_view->save();
   }
-  else {
-    $view_contains_content_lock = FALSE;
-    foreach (array_keys($content_view->get('display')) as $display_name) {
-      $display = $content_view->getDisplay($display_name);
-      $options = ['fields', 'filters', 'sorts', 'relationships'];
-      foreach ($options as $option) {
-        if (!empty($display['display_options'][$option]) && is_array($display['display_options'][$option])) {
-          foreach ($display['display_options'][$option] as $key => $values) {
-            if ($values['table'] == 'content_lock') {
-              $view_contains_content_lock = TRUE;
-            }
-          }
-        }
-      }
-    }
 
-    if ($view_contains_content_lock) {
-      // Duplicate content into content_content_lock view and add third party
-      // settings.
-      $content_lock_view = $content_view->createDuplicate();
-      $content_lock_view->set('id', 'content_content_lock');
-      $content_lock_view->setThirdPartySetting('config_selector', 'feature', 'thunder_content_view');
-      $content_lock_view->setThirdPartySetting('config_selector', 'priority', 1);
-      $content_lock_view->save();
-
-      // Delete all content_lock related stuff from the content view.
-      foreach (array_keys($content_view->get('display')) as $display_name) {
-        $display = &$content_view->getDisplay($display_name);
-
-        // Delete content_lock relationships.
-        $deleted_relationships = [];
-        if (!empty($display['display_options']['relationships']) && is_array($display['display_options']['relationships'])) {
-          foreach ($display['display_options']['relationships'] as $key => $relationship) {
-            if ($relationship['table'] == 'content_lock') {
-              unset($display['display_options']['relationships'][$key]);
-              $deleted_relationships[] = $key;
-            }
-          }
-        }
-
-        // Delete content lock fields, filters and sorts and those that depend
-        // on a deleted relationship.
-        $options = ['fields', 'filters', 'sorts'];
-        foreach ($options as $option) {
-          if (!empty($display['display_options'][$option]) && is_array($display['display_options'][$option])) {
-            foreach ($display['display_options'][$option] as $key => $values) {
-              if ($values['table'] == 'content_lock' || (!empty($values['relationship']) && in_array($values['relationship'], $deleted_relationships))) {
-                unset($display['display_options'][$option][$key]);
-              }
-            }
-          }
-        }
-      }
-      $content_view->disable();
-    }
-    else {
-      /** @var \Drupal\config_update\ConfigReverter $config_service */
-      $config_service = \Drupal::service('config_update.config_update');
-      $config_service->import('view', 'content_content_lock');
-      $content_lock_view = View::load('content_content_lock');
-      $content_lock_view->disable();
-      $content_lock_view->save();
-    }
-  }
   $content_view->save();
 }
