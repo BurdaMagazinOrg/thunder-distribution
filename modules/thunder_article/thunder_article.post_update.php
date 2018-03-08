3c75d72448fa83a9fae819622d5c51292071e105
Christian Fritsch <chr.fritsch@gmx.net>
2018-03-08 10:50:04 +0100

    Add update checklist point

--- modules/thunder_article/thunder_article.post_update.php
+++ modules/thunder_article/thunder_article.post_update.php
@@ -17,16 +17,20 @@ function thunder_article_post_update_move_scheduler_to_content_list() {
 
   /** @var \Drupal\config_update\ConfigReverter $config_update */
   $config_update = \Drupal::service('config_update.config_update');
-  $config_update->import('system.simple', 'thunder_article.settings');
-
-  $view = View::load('locked_content');
-  if ($view) {
-    $view->set('id', 'locked_content_old');
-    $view->save();
-    $thunderUpdater->logger()->alert('The locked content view was renamed to views.view.locked_content_old.');
+  if ($config_update->import('system.simple', 'thunder_article.settings')) {
+    $view = View::load('locked_content');
+    if ($view) {
+      $view->set('id', 'locked_content_old');
+      $view->save();
+      $thunderUpdater->logger()->alert('The locked content view was renamed to views.view.locked_content_old.');
+    }
+    if ($config_update->import('view', 'locked_content')) {
+      $thunderUpdater->logger()->info('The new locked content view was imported as views.view.locked_content.');
+      $thunderUpdater->checklist()->markUpdatesSuccessful(['thunder_article_move_scheduler_to_content_list']);
+    }
   }
-  $config_update->import('view', 'locked_content');
-  $thunderUpdater->logger()->info('The new locked content view was imported as views.view.locked_content.');
+
+
 
   // Output logged messages to related channel of update execution.
   return $thunderUpdater->logger()->output();
