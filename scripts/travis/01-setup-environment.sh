304a620abd17e55ad237254b37d8b84c5a79e8db
Daniel Bosen <daniel.bosen@burda.com>
2018-03-12 12:19:53 +0100

    Simplify default values

--- scripts/travis/01-setup-environment.sh
+++ scripts/travis/01-setup-environment.sh
@@ -23,20 +23,18 @@ export UPDATE_BASE_PATH=${TEST_DIR}-update-base
 
 
 # Get latest version of imagick from api.github.com
-PHP_IMAGICK_VERSION=`curl -L -s -H 'Accept: application/json' https://api.github.com/repos/mkoppanen/imagick/tags | jq -r '.[0].name'`
-export PHP_IMAGICK_VERSION
-export PHP_IMAGICK_VERSION_DEFAULT="3.4.3"
+PHP_IMAGICK_VERSION_FROM_API=`curl -L -s -H 'Accept: application/json' https://api.github.com/repos/mkoppanen/imagick/tags | jq -r '.[0].name'`
+export PHP_IMAGICK_VERSION=${PHP_IMAGICK_VERSION_FROM_API:-3.4.3}
 
 # Get latest version of Yaml PHP library (for PHP 5.6 -> Yaml version 1.x will be used)
 if [[ $TRAVIS_PHP_VERSION = '5.6' ]] ; then
   PHP_YAML_VERSION_DEFAULT="1.3.1"
-  PHP_YAML_VERSION=`curl -L -s -H 'Accept: application/json' https://api.github.com/repos/php/pecl-file_formats-yaml/tags | jq -r '[ .[].name | select(index("1.")==0) ] | .[0]'`
+  PHP_YAML_VERSION_FROM_API=`curl -L -s -H 'Accept: application/json' https://api.github.com/repos/php/pecl-file_formats-yaml/tags | jq -r '[ .[].name | select(index("1.")==0) ] | .[0]'`
 else
-PHP_YAML_VERSION_DEFAULT="2.0.2"
-  PHP_YAML_VERSION=`curl -L -s -H 'Accept: application/json' https://api.github.com/repos/php/pecl-file_formats-yaml/tags | jq -r '.[0].name'`
+  PHP_YAML_VERSION_DEFAULT="2.0.2"
+  PHP_YAML_VERSION_FROM_API=`curl -L -s -H 'Accept: application/json' https://api.github.com/repos/php/pecl-file_formats-yaml/tags | jq -r '.[0].name'`
 fi;
-export PHP_YAML_VERSION
-export PHP_YAML_VERSION_DEFAULT
+export PHP_YAML_VERSION=${PHP_YAML_VERSION_FROM_API:-PHP_YAML_VERSION_DEFAULT}
 
 # Set a default install method if none set.
 if [[ ${INSTALL_METHOD} == "" ]]; then
--- scripts/travis/02-before-install.sh
+++ scripts/travis/02-before-install.sh
@@ -25,13 +25,13 @@ drush dl drupalorg_drush-7.x
 drush verify-makefile
 
 # install image magick
-wget https://github.com/mkoppanen/imagick/archive/${PHP_IMAGICK_VERSION:-$PHP_IMAGICK_VERSION_DEFAULT}.tar.gz -O php-imagick-LATEST.tar.gz
+wget https://github.com/mkoppanen/imagick/archive/$PHP_IMAGICK_VERSION.tar.gz -O php-imagick-LATEST.tar.gz
 yes '' | pecl install -f php-imagick-LATEST.tar.gz
 
 # Build and install the YAML extension for strict parsing.
-wget https://github.com/php/pecl-file_formats-yaml/archive/${PHP_YAML_VERSION:-$PHP_YAML_VERSION_DEFAULT}.tar.gz -O php-yaml-LATEST.tar.gz
+wget https://github.com/php/pecl-file_formats-yaml/archive/$PHP_YAML_VERSION.tar.gz -O php-yaml-LATEST.tar.gz
 tar -C /tmp -zxvf php-yaml-LATEST.tar.gz
-cd /tmp/pecl-file_formats-yaml-${PHP_YAML_VERSION:-$PHP_YAML_VERSION_DEFAULT}
+cd /tmp/pecl-file_formats-yaml-$PHP_YAML_VERSION
 phpize
 ./configure
 make
