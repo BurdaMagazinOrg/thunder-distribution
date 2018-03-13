efeaf52548daa8912acd85b49e16a1f203921b39
Christian Fritsch <chr.fritsch@gmx.net>
2018-03-13 10:00:31 +0100

    Make search integration not enabled by default

--- src/Plugin/Thunder/OptionalModule/ThunderSearch.php
+++ src/Plugin/Thunder/OptionalModule/ThunderSearch.php
@@ -10,7 +10,6 @@ namespace Drupal\thunder\Plugin\Thunder\OptionalModule;
  *   label = @Translation("Thunder Search integration"),
  *   description = @Translation("Better search experience for editors and users"),
  *   type = "module",
- *   standardlyEnabled = 1,
  *   weight = -1
  * )
  */
