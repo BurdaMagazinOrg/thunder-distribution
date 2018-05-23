7d3856b09ddc382dcc8af16f17a751103db1cb3c
Christian Fritsch <chr.fritsch@gmx.net>
2018-05-23 11:30:43 +0200

    Move update hook into update_N

--- thunder.post_update.php
+++ thunder.post_update.php
@@ -4,14 +4,3 @@
  * @file
  * Post update functions for Thunder.
  */
-
-/**
- * Install content_moderation and scheduler integration.
- */
-function thunder_post_update_enable_content_moderation() {
-  /** @var \Drupal\thunder_updater\Updater $thunderUpdater */
-  $thunderUpdater = \Drupal::service('thunder_updater');
-  $thunderUpdater->installModules(['thunder_enable_content_moderation' => 'scheduler_content_moderation_integration']);
-  // Output logged messages to related channel of update execution.
-  return $thunderUpdater->logger()->output();
-}
