c3c50aafca19fefaa9950f2a3591e98097c5ac97
Christian Fritsch <chr.fritsch@gmx.net>
2018-08-03 09:33:11 +0200

    Revert "Remove the suppress-deprecations"
    This reverts commit 0107589015a04f2512d90855ec3087a0c7c6aa95.

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -5,13 +5,13 @@ cd ${TEST_DIR}/docroot
 
 # disable configuration testing for update test path
 if [[ ${TEST_UPDATE} != "true" ]]; then
-    thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 ThunderConfig
+    thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 ThunderConfig
 fi
 
 # execute Drupal tests
-thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 Thunder,thunder_updater
+thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 Thunder,thunder_updater
 
 if [[ ${TEST_UPDATE} == "true" || ${TEST_INSTALLER} == "true" ]]; then
-    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 --class "Drupal\thunder\Tests\Installer\ThunderInstallerTest"
-    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --verbose --color --url http://localhost:8080 --class "Drupal\thunder\Tests\Installer\ThunderInstallerGermanTest"
+    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 --class "Drupal\thunder\Tests\Installer\ThunderInstallerTest"
+    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 --class "Drupal\thunder\Tests\Installer\ThunderInstallerGermanTest"
 fi
