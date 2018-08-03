60dd67d5ef120e91f286d75fac99f354ac776239
Christian Fritsch <chr.fritsch@gmx.net>
2018-08-03 10:20:02 +0200

    [TEST_UPDATE=true] Trigger update test path

--- src/Tests/Installer/ThunderInstallerTest.php
+++ src/Tests/Installer/ThunderInstallerTest.php
@@ -87,10 +87,6 @@ class ThunderInstallerTest extends InstallerTestBase {
     // Configure modules.
     $this->setUpModules();
 
-    // Click link, we don't get redirected automatically in tests.
-    $this->clickLink('click here');
-    $this->isInstalled = TRUE;
-
     if ($this->isInstalled) {
       // Import new settings.php written by the installer.
       $request = Request::createFromGlobals();
