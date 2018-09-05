d1513eb68162a363ed9b1c87626ca98c60af9453
Christian Fritsch <chr.fritsch@gmx.net>
2018-09-05 14:49:04 +0200

    Fix cs issue

--- src/Plugin/Field/FieldWidget/ModerationStateWidget.php
+++ src/Plugin/Field/FieldWidget/ModerationStateWidget.php
@@ -7,6 +7,8 @@ use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
+ * Plugin implementation of the 'thunder_moderation_state_default' widget.
+ *
  * Thunder provides it's own moderation_state widget that only shows a list of
  * possible states to switch in. To display the current state is not part of
  * this widget. Thunder shows that in ThunderNodeForm.
