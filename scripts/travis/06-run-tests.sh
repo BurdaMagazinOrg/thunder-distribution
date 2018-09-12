b32357002a1a39a04d6cfeaea1b3c01b31da5cf4
Daniel Bosen <daniel.bosen@burda.com>
2018-09-12 10:05:02 +0200

    run all tests

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -9,7 +9,7 @@ if [[ ${TEST_UPDATE} != "true" ]]; then
 fi
 
 # execute Drupal tests
-thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 Thunder_restricted
+thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 Thunder,thunder_updater
 
 if [[ ${TEST_UPDATE} == "true" || ${TEST_INSTALLER} == "true" ]]; then
     php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 --class "Drupal\Tests\thunder\Functional\Installer\ThunderInstallerTest"
