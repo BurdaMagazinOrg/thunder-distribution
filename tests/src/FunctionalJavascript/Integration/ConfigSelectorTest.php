9b9daf7635f5794545540467b2f62e1c674b75fe
Christian Fritsch <chr.fritsch@gmx.net>
2018-10-25 11:16:33 +0200

    Fix test

--- tests/src/FunctionalJavascript/Integration/ConfigSelectorTest.php
+++ tests/src/FunctionalJavascript/Integration/ConfigSelectorTest.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace Drupal\Tests\thunder\Functional\Integration;
+namespace Drupal\Tests\thunder\FunctionalJavascript\Integration;
 
-use Drupal\Tests\thunder\Functional\ThunderTestBase;
 use Drupal\Tests\thunder\FunctionalJavascript\ThunderJavascriptTestBase;
 
 /**
@@ -14,12 +13,12 @@ class ConfigSelectorTest extends ThunderJavascriptTestBase {
 
   protected static $modules = ['thunder_demo'];
 
+  protected static $defaultUserRole = 'administrator';
+
   /**
    * Tests content view with and without search_api.
    */
   public function testContentViewSearchApi() {
-
-    $this->logWithRole('administrator');
 
     $assert_session = $this->assertSession();
 
@@ -29,23 +28,20 @@ class ConfigSelectorTest extends ThunderJavascriptTestBase {
     $assert_session->elementExists('css', '#block-thunder-admin-content > div > div.view-content');
 
     // Install thunder_search.
-    $this->drupalPostForm('admin/modules', ['modules[thunder_search][enable]' => 1], 'Install');
-    $this->drupalGet('admin/config/search/search-api/index/content');
-    $this->click('#edit-submit');
+    \Drupal::service('module_installer')->install(['thunder_search']);
 
     // Now we have a search_api based view.
     $this->drupalGet('admin/config/search/search-api/index/content');
-    $this->getSession()->getPage()->pressButton('Track now');
+    $this->getSession()->getPage()->pressButton('Index now');
+    $assert_session->waitForId('edit-index-now');
 
-
-    $this->drupalGet('admin/config/search/search-api/index/content');
-    #$this->drupalGet('admin/content');
+    $this->drupalGet('admin/content');
     $assert_session->elementExists('xpath', '//*[@id="view-label-table-column"]/a');
     $assert_session->elementExists('css', '#block-thunder-admin-content > div > div.view-content-search-api');
 
     // Uninstall search_api.
-    $this->drupalPostForm('admin/modules/uninstall', ['uninstall[thunder_search]' => 1, 'uninstall[search_api]' => 1], 'Uninstall');
-    $this->click('#edit-submit');
+    \Drupal::service('module_installer')->uninstall(['thunder_search', 'search_api']);
+    drupal_flush_all_caches();
 
     // The normal view is back.
     $this->drupalGet('admin/content');
