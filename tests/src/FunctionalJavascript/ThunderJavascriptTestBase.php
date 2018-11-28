b51a1b02a6bdf1698b256745e93e610b7173f86c
Christian Fritsch <chr.fritsch@gmx.net>
2018-11-28 17:46:17 +0100

    Revert "Remove more stuff"
    This reverts commit 7adcaf860c5f5e26dc8563dbb0bdf8cfd4cdd2a2.

--- tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\thunder\FunctionalJavascript;
 
+use Behat\Mink\Driver\Selenium2Driver;
 use Behat\Mink\Element\DocumentElement;
 use Behat\Mink\Exception\DriverException;
 use Drupal\FunctionalJavascriptTests\DrupalSelenium2Driver;
@@ -282,6 +283,24 @@ abstract class ThunderJavascriptTestBase extends WebDriverTestBase {
 
     $page->pressButton($fieldName);
     $this->assertSession()->assertWaitOnAjaxRequest();
+  }
+
+  /**
+   * Assert page title.
+   *
+   * @param string $expectedTitle
+   *   Expected title.
+   */
+  protected function assertPageTitle($expectedTitle) {
+    $driver = $this->getSession()->getDriver();
+    if ($driver instanceof Selenium2Driver) {
+      $actualTitle = $driver->getWebDriverSession()->title();
+
+      static::assertEquals($expectedTitle, $actualTitle, 'Title found');
+    }
+    else {
+      $this->assertSession()->titleEquals($expectedTitle);
+    }
   }
 
   /**
