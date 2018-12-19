49e1bc29a3f379b967b0ccda1fc914e3ba89a147
Daniel Bosen <daniel.bosen@burda.com>
2018-12-19 10:50:04 +0100

    [TEST_UPDATE=true] fix example

--- scripts/travis/01-setup-environment.sh
+++ scripts/travis/01-setup-environment.sh
@@ -34,10 +34,10 @@ fi;
 
 # Manual overrides of environment variables by commit messages. To override a variable add something like this to
 # your commit message:
-# git commit -m="Your commit message [TEST_UPDATE=true]"
+# git commit -m"Your commit message [TEST_UPDATE=true]"
 #
 # To override multiple variables us something like this:
-# git commit -m="Your other commit message [TEST_UPDATE=true|INSTALL_METHOD=composer]"
+# git commit -m"Your other commit message [TEST_UPDATE=true|INSTALL_METHOD=composer]"
 if [[ ${TRAVIS_EVENT_TYPE} == "pull_request" ]]; then
     # These are the variables, that are allowed to be overridden
     ALLOWED_VARIABLES=("TEST_UPDATE" "INSTALL_METHOD" "TEST_INSTALLER" "SAUCE_LABS_ENABLED")
