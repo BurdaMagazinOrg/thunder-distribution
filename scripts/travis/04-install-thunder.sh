dcf6a6f0a70f8aa28f98a88db81c6a97825d84d7
Christian Fritsch <chr.fritsch@gmx.net>
2019-01-14 09:25:31 +0100

    Use fix/3025280-entity-browser-z-index branch

--- scripts/travis/04-install-thunder.sh
+++ scripts/travis/04-install-thunder.sh
@@ -35,7 +35,7 @@ drush_make_thunder() {
 
     # Get development branch of Thunder Admin theme (to use same admin theme as for composer build)
     rm -rf ${TEST_DIR}/docroot/profiles/thunder/themes/thunder_admin
-    git clone --depth 1 --single-branch --branch 8.x-2.x https://git.drupal.org/project/thunder_admin.git ${TEST_DIR}/docroot/profiles/thunder/themes/thunder_admin
+    git clone --depth 1 --single-branch --branch fix/3025280-entity-browser-z-index https://git.drupal.org/project/thunder_admin.git ${TEST_DIR}/docroot/profiles/thunder/themes/thunder_admin
 
     composer install --working-dir=${TEST_DIR}/docroot
     composer run-script drupal-phpunit-upgrade --working-dir=${TEST_DIR}/docroot
@@ -52,7 +52,7 @@ composer_create_thunder() {
     fi
 
     composer config repositories.thunder path ${THUNDER_DIST_DIR}
-    composer require "burdamagazinorg/thunder:*" "drupal/thunder_admin:dev-2.x" --no-progress
+    composer require "burdamagazinorg/thunder:*" "drupal/thunder_admin:dev-fix/3025280-entity-browser-z-index" --no-progress
 }
 
 apply_patches() {
