7d9f5ebdd0da385672a458e96003c984c59899a2
Christian Fritsch <chr.fritsch@gmx.net>
2019-02-07 11:45:25 +0100

    Simplify code

--- src/Plugin/views/area/FacetBlock.php
+++ src/Plugin/views/area/FacetBlock.php
@@ -5,7 +5,6 @@ namespace Drupal\thunder\Plugin\views\area;
 use Drupal\Core\Block\BlockManagerInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\facets\FacetManager\DefaultFacetManager;
-use Drupal\facets\FacetSource\FacetSourcePluginManager;
 use Drupal\views\Plugin\views\area\AreaPluginBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -20,8 +19,6 @@ class FacetBlock extends AreaPluginBase {
 
   protected $facetManager;
 
-  protected $facetSourceManager;
-
   protected $blockManager;
 
   protected $currentUser;
@@ -29,10 +26,9 @@ class FacetBlock extends AreaPluginBase {
   /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, DefaultFacetManager $facetManager, FacetSourcePluginManager $facetSourceManager, BlockManagerInterface $blockManager, AccountInterface $account) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, DefaultFacetManager $facetManager, BlockManagerInterface $blockManager, AccountInterface $account) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->facetManager = $facetManager;
-    $this->facetSourceManager = $facetSourceManager;
     $this->blockManager = $blockManager;
     $this->currentUser = $account;
   }
@@ -41,23 +37,13 @@ class FacetBlock extends AreaPluginBase {
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition, $container->get('facets.manager'), $container->get('plugin.manager.facets.facet_source'), $container->get('plugin.manager.block'), $container->get('current_user'));
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('facets.manager'), $container->get('plugin.manager.block'), $container->get('current_user'));
   }
 
   /**
    * {@inheritdoc}
    */
   public function render($empty = FALSE) {
-
-    $facet_source = NULL;
-    foreach ($this->facetSourceManager->getDefinitions() as $definition) {
-      /** @var \Drupal\facets\Plugin\facets\facet_source\SearchApiDisplay $source */
-      $source = $this->facetSourceManager->createInstance($definition['id']);
-      if ($this->view->id() == $source->getViewsDisplay()->id() && $this->view->current_display == $source->getViewsDisplay()->current_display) {
-        $facet_source = $source;
-        break;
-      }
-    }
 
     $build = [
       '#type' => 'container',
@@ -66,10 +52,12 @@ class FacetBlock extends AreaPluginBase {
     ];
     $build['exposed_form']['#attributes']['class'][] = 'form-item';
 
-    if (!$facet_source || !$this->view->result) {
+    if (!$this->view->result) {
       return $build;
     }
-    foreach ($this->facetManager->getFacetsByFacetSourceId($facet_source->pluginId) as $id => $facet) {
+
+    $facets_source_plugin_id = 'search_api:views_' . $this->view->getDisplay()->getPluginId() . '__' . $this->view->id() . '__' . $this->view->current_display;
+    foreach ($this->facetManager->getFacetsByFacetSourceId($facets_source_plugin_id) as $id => $facet) {
       /** @var \Drupal\Core\Block\BlockPluginInterface $plugin_block */
       $plugin_block = $this->blockManager->createInstance('facet_block:' . $facet->id(), []);
 
