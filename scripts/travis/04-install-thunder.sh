1acfc40a6196e4120af9b0669b97a1fa0cb45ab9
Christian Fritsch <chr.fritsch@gmx.net>
2019-02-25 13:32:52 +0100

    Back to black

--- scripts/travis/04-install-thunder.sh
+++ scripts/travis/04-install-thunder.sh
@@ -63,7 +63,7 @@ drush_make_thunder() {
 
 composer_create_thunder() {
     cd ${THUNDER_DIST_DIR}
-    composer create-project burdamagazinorg/thunder-project:dev-feature/update-project ${TEST_DIR} --stability dev --no-interaction --no-install
+    composer create-project burdamagazinorg/thunder-project:2.x ${TEST_DIR} --stability dev --no-interaction --no-install
 
     cd ${TEST_DIR}
 
