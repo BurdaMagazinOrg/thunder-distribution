79f1476709375f5986b9c5480f5c91a0f9b2c22d
Christian Fritsch <chr.fritsch@gmx.net>
2019-02-27 13:40:07 +0100

    Foo

--- scripts/travis/02-before-install.sh
+++ scripts/travis/02-before-install.sh
@@ -6,9 +6,6 @@ echo "" > ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
 echo "sendmail_path = /bin/true" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 phpenv rehash
 
-# Install Drush
-composer global require drush/drush:^8.1
-
 # Prepare test directory
 cd $TRAVIS_BUILD_DIR
 mkdir -p ${TEST_DIR}
