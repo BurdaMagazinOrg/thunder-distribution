ed685cbbe657c65640cea10cd9bca008a5c66964
Christian Fritsch <chr.fritsch@gmx.net>
2019-02-27 16:36:08 +0100

    Foo

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -18,11 +18,11 @@ fi
 
 # disable configuration testing for update test path
 if [[ ${TEST_UPDATE} != "true" ]]; then
-    thunderDumpFile=thunder.php paratest --phpunit=/home/travis/build/BurdaMagazinOrg/thunder-distribution/../test-dir/bin/phpunit ${THUNDER_DIST_DIR}/tests --verbose --color --group ThunderConfig
+    thunderDumpFile=thunder.php paratest --phpunit=/home/travis/build/BurdaMagazinOrg/thunder-distribution/../test-dir/bin/phpunit ${THUNDER_DIST_DIR}/tests --colors --functional --group ThunderConfig
 fi
 
 # execute Drupal tests
-thunderDumpFile=thunder.php paratest --phpunit=/home/travis/build/BurdaMagazinOrg/thunder-distribution/../test-dir/bin/phpunit ${THUNDER_DIST_DIR}/tests --verbose --color --group Thunder,thunder_updater
+thunderDumpFile=thunder.php paratest --phpunit=/home/travis/build/BurdaMagazinOrg/thunder-distribution/../test-dir/bin/phpunit ${THUNDER_DIST_DIR}/tests --colors --functional --group Thunder,thunder_updater
 
 if [[ ${TEST_UPDATE} == "true" || ${TEST_INSTALLER} == "true" ]]; then
     php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 --class "Drupal\Tests\thunder\Functional\Installer\ThunderInstallerTest"
