ceb85b4ae31cdd90080bd9ae102cb7b2812755a0
Christian Fritsch <chr.fritsch@gmx.net>
2019-02-27 14:46:33 +0100

    Foo

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -16,11 +16,11 @@ fi
 
 # disable configuration testing for update test path
 if [[ ${TEST_UPDATE} != "true" ]]; then
-    thunderDumpFile=thunder.php php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 ThunderConfig
+    thunderDumpFile=thunder.php php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --concurrency 2 --url http://localhost:8080 ThunderConfig
 fi
 
 # execute Drupal tests
-thunderDumpFile=thunder.php php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 Thunder,thunder_updater
+thunderDumpFile=thunder.php php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color  --concurrency 2 --url http://localhost:8080 Thunder,thunder_updater
 
 if [[ ${TEST_UPDATE} == "true" || ${TEST_INSTALLER} == "true" ]]; then
     php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 --class "Drupal\Tests\thunder\Functional\Installer\ThunderInstallerTest"
