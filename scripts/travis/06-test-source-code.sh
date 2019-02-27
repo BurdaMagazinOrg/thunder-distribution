46a3856f7a5b22a433bd9c3656a501b2c4dd4f64
Christian Fritsch <chr.fritsch@gmx.net>
2019-02-27 13:26:00 +0100

    Foo

--- scripts/travis/06-test-source-code.sh
+++ scripts/travis/06-test-source-code.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-pwd
-
 # run phpcs
-phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
+phpcs --config-set installed_paths ${TEST_DIR}/vendor/drupal/coder/coder_sniffer
 # Ignore check of .md files, because they should be able to contain more then 80 characters per line.
 phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=*.md -p .
 phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme -p .
