19b512d96c4523d8ab11fe7a92ae8f4ce62c865a
Christian Fritsch <chr.fritsch@gmx.net>
2019-03-11 10:13:47 +0100

    Revert paratest

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -3,8 +3,6 @@
 # Run Drupal tests (@group Thunder)
 cd ${TEST_DIR}/docroot
 
-cp ${THUNDER_DIST_DIR}/scripts/travis/phpunit.xml.dist phpunit.xml
-
 # Make simple export import
 if [[ "${TEST_UPDATE}" != "true" && "${TEST_DEPLOYMENT}" == "true" ]]; then
     drush -y cex sync
@@ -18,12 +16,13 @@ fi
 
 # disable configuration testing for update test path
 if [[ ${TEST_UPDATE} != "true" ]]; then
-    thunderDumpFile=thunder.php paratest --phpunit=/home/travis/build/BurdaMagazinOrg/thunder-distribution/../test-dir/bin/phpunit ${THUNDER_DIST_DIR}/tests --runner WrapperRunner -p2 --group ThunderConfig
+    thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 ThunderConfig
 fi
 
 # execute Drupal tests
-thunderDumpFile=thunder.php paratest --phpunit=/home/travis/build/BurdaMagazinOrg/thunder-distribution/../test-dir/bin/phpunit ${THUNDER_DIST_DIR}/tests --runner WrapperRunner -p2 --group Thunder,thunder_updater
+thunderDumpFile=thunder.php.gz php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 Thunder,thunder_updater
 
 if [[ ${TEST_UPDATE} == "true" || ${TEST_INSTALLER} == "true" ]]; then
-    thunderDumpFile=thunder.php paratest --phpunit=/home/travis/build/BurdaMagazinOrg/thunder-distribution/../test-dir/bin/phpunit ${THUNDER_DIST_DIR}/tests --runner WrapperRunner -p2 --group ThunderInstaller
+    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 --class "Drupal\Tests\thunder\Functional\Installer\ThunderInstallerTest"
+    php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 --class "Drupal\Tests\thunder\Functional\Installer\ThunderInstallerGermanTest"
 fi
