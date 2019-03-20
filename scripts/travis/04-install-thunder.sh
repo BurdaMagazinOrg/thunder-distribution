28f4efd57a4ecdf5b0bc730b0fc92507b1dfe4a5
Volker Killesreiter <killesre@gmail.com>
2019-03-20 17:20:40 +0100

    Revert "REVERT BEFORE COMMIT: switch to thunder_admin branch"
    This reverts commit a06ab073fdedc624bb08a3c1b909d495b0d4f743.

--- scripts/travis/04-install-thunder.sh
+++ scripts/travis/04-install-thunder.sh
@@ -74,7 +74,7 @@ composer_create_thunder() {
     fi
 
     composer config repositories.thunder path ${THUNDER_DIST_DIR}
-    composer require "burdamagazinorg/thunder:*" "drupal/thunder_admin:dev-feature/3041804-layout_thunder_editorial_search_with_search_api" "mglaman/phpstan-drupal" "phpstan/phpstan-deprecation-rules" --no-progress
+    composer require "burdamagazinorg/thunder:*" "drupal/thunder_admin:dev-2.x" "mglaman/phpstan-drupal" "phpstan/phpstan-deprecation-rules" --no-progress
 }
 
 apply_patches() {
