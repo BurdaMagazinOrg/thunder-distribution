51213e31d185ea7e3548bb1a94ad780d3cd65afc
Christian Fritsch <chr.fritsch@gmx.net>
2019-03-22 15:55:45 +0100

    Speed

--- scripts/travis/06-run-config-tests.sh
+++ scripts/travis/06-run-config-tests.sh
@@ -3,5 +3,5 @@
 # Run Drupal tests
 cd ${TEST_DIR}/docroot
 
-php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 ThunderConfig
+php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --dburl mysql://travis@127.0.0.1/drupal --verbose --color --url http://localhost:8080 ThunderConfig
 #thunderDumpFile=thunder.php php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 ThunderConfig
