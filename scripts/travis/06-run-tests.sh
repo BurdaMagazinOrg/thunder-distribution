55c6d2cc6c3c4a05dfa77ff6340ca303a6b460ab
Christian Fritsch <chr.fritsch@gmx.net>
2019-03-22 14:50:16 +0100

    Back
    Add config test
    
    Foo

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -14,11 +14,6 @@ if [[ "${TEST_UPDATE}" != "true" ]]; then
     fi
 fi
 
-# disable configuration testing for update test path
-if [[ ${TEST_UPDATE} != "true" ]]; then
-    thunderDumpFile=thunder.php php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 ThunderConfig
-fi
-
 # execute Drupal tests
 thunderDumpFile=thunder.php php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 Thunder
 
