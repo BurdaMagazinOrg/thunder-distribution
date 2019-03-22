9aeb4abfee6dcc0b345f7b7f740183d3427908a5
Christian Fritsch <chr.fritsch@gmx.net>
2019-03-22 15:08:23 +0100

    Lets see

--- scripts/travis/04-install-thunder.sh
+++ scripts/travis/04-install-thunder.sh
@@ -74,7 +74,7 @@ composer_create_thunder() {
     fi
 
     composer config repositories.thunder path ${THUNDER_DIST_DIR}
-    composer require "burdamagazinorg/thunder:*" "drupal/thunder_admin:dev-2.x" "mglaman/phpstan-drupal" "phpstan/phpstan-deprecation-rules" --no-progress
+    composer require "burdamagazinorg/thunder:*" "drupal/thunder_admin:dev-2.x" "mglaman/phpstan-drupal" "phpstan/phpstan-deprecation-rules" "drupal/core:${DRUPAL_CORE}" --no-progress
 
      # Get custom branch of Thunder Admin theme
     rm -rf ${TEST_DIR}/docroot/themes/contrib/thunder_admin
