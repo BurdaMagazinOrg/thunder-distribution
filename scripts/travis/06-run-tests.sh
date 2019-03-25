5662d7a6c4b8fdbe2989f982eb02a3368ec42792
Christian Fritsch <chr.fritsch@gmx.net>
2019-03-25 09:41:25 +0100

    Revert "Move dump creation"
    This reverts commit 2ef67f80e8f973ea962b83231d2c043c7d7d5dda.

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-create_testing_dump
-
 cd ${TEST_DIR}/docroot
 
 docker run -d -p 4444:4444 -v $(pwd)/$(drush eval "echo drupal_get_path('profile', 'thunder');")/tests:/tests -v /dev/shm:/dev/shm --net=host selenium/standalone-chrome:3.14.0-iron
