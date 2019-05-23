00ea4208305b5f8411c747061bcc8adb88d9b12a
Daniel Bosen <daniel.bosen@burda.com>
2019-05-23 12:40:00 +0200

    Increase composer memory limit

--- scripts/travis/03-build-thunder-composer.sh
+++ scripts/travis/03-build-thunder-composer.sh
@@ -19,7 +19,7 @@ if [[ ${TEST_UPDATE} == "true" ]]; then
 fi
 
 composer config repositories.thunder path ${THUNDER_DIST_DIR}
-composer require "burdamagazinorg/thunder:*" "mglaman/phpstan-drupal" "phpstan/phpstan:0.11.6"  "nette/di:*@stable" "phpstan/phpstan-deprecation-rules" --no-progress
+COMPOSER_MEMORY_LIMIT=-1 composer require "burdamagazinorg/thunder:*" "mglaman/phpstan-drupal" "phpstan/phpstan:0.11.6"  "nette/di:*@stable" "phpstan/phpstan-deprecation-rules" --no-progress
 
  # Get custom branch of Thunder Admin theme
 rm -rf ${TEST_DIR}/docroot/themes/contrib/thunder_admin
