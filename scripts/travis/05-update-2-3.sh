5231b587d46c6f49850657a9a6f3a3c542364b50
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-25 10:44:38 +0200

    Use stable media_entity in upgrade path testing

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -14,10 +14,10 @@ composer require "thunder/thunder-distribution:*" --no-update
 
 composer update
 
-jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2019-06-04/2997799-25.patch"}, "drupal/media_entity":{"media_entity_update_8201() doesnt delete fields correctly":"https://www.drupal.org/files/issues/2019-06-17/3062219-4_0.patch"}}' composer.json > composer1.json
+jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2019-06-04/2997799-25.patch"}}' composer.json > composer1.json
 mv composer1.json composer.json
 
-composer require "drupal/media_entity:2.x-dev" "drupal/video_embed_field:^2.0" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
+composer require "drupal/media_entity:^2.0-beta4" "drupal/video_embed_field:^2.0" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
 
 cd ${TEST_DIR}/docroot
 drush cr
