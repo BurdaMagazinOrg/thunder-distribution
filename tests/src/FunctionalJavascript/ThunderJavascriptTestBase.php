8e48cca3c52a6402679eb8d90b120f8a590b3ecc
Christian Fritsch <chr.fritsch@gmx.net>
2019-07-04 12:51:22 +0200

    Issue #3065838 by chr.fritsch, daniel.bosen: Update to Drupal 8.7.4 and adopt test changes

--- tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/ThunderJavascriptTestBase.php
@@ -68,14 +68,21 @@ abstract class ThunderJavascriptTestBase extends WebDriverTestBase {
 
     $this->logWithRole(static::$defaultUserRole);
 
-    // Set window width/height.
-    $windowSize = $this->getWindowSize();
-    $this->getSession()->getDriver()->resizeWindow($windowSize['width'], $windowSize['height']);
-
     // Set flag to generate screenshots instead of comparing them.
     if (!empty($_SERVER['generateMode'])) {
       $this->setGenerateMode(strtolower($_SERVER['generateMode']) === 'true');
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function initFrontPage() {
+    parent::initFrontPage();
+    // Set a standard window size so that all javascript tests start with the
+    // same viewport.
+    $windowSize = $this->getWindowSize();
+    $this->getSession()->resizeWindow($windowSize['width'], $windowSize['height']);
   }
 
   /**
