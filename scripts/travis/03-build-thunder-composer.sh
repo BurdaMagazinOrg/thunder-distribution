59ccf22917639b33dc1720e01c4a2390231d69ba
Volker Killesreiter <killesre@gmail.com>
2020-03-04 11:03:44 +0100

    Revert "composer repository handling"
    This reverts commit 49b8bb961b99018387efe8e659f2b6e787e9dc47.

--- scripts/travis/03-build-thunder-composer.sh
+++ scripts/travis/03-build-thunder-composer.sh
@@ -24,14 +24,9 @@ cd ${TEST_DIR}
 if [[ ${TEST_UPDATE} == "true" ]]; then
     sed -i 's/docroot\/profiles\/contrib/docroot\/profiles/g' composer.json
 fi
+composer config repositories.nexx_integration git https://github.com/BurdaMagazinOrg/module-nexx_integration.git
 
 composer config repositories.thunder path ${THUNDER_DIST_DIR}
-composer config repositories.nexx_integration git https://github.com/BurdaMagazinOrg/module-nexx_integration.git
-composer config repositories.0 --unset
-composer config repositories.1 --unset
-composer config repositories.drupal composer https://packages.drupal.org/8
-composer config repositories.assets composer https://asset-packagist.org
-
 COMPOSER_MEMORY_LIMIT=-1 composer require "burdamagazinorg/thunder:*" "thunder/thunder_testing_demo:2.x-dev" "mglaman/phpstan-drupal:~0.11.11" "phpstan/phpstan-deprecation-rules:~0.11.0" "drupal/nexx_integration:dev-fix/paragraphs-config-schema" --no-progress
 
  # Get custom branch of Thunder Admin theme
